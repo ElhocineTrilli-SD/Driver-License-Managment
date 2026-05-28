998c4d4cd616deac0661723252f2208f1dc0d4af
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-05-28 09:40:17 +0100

    added new user controll to find and add  person  by Id or nationalno

---
+++ DVLD/People/Controlls/PersonInfoWithFilter.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace DVLD.People.Controlls
+{
+    partial class PersonInfoWithFilter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbPersonFilter = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilterValue = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnAddNewPerson = new Guna.UI2.WinForms.Guna2Button();
+            this.btnSearch = new Guna.UI2.WinForms.Guna2Button();
+            this.cbFilterBy = new System.Windows.Forms.ComboBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.personUserControl11 = new DVLD.PersonUserControl1();
+            this.gbPersonFilter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbPersonFilter
+            // 
+            this.gbPersonFilter.Controls.Add(this.cbFilterBy);
+            this.gbPersonFilter.Controls.Add(this.btnAddNewPerson);
+            this.gbPersonFilter.Controls.Add(this.btnSearch);
+            this.gbPersonFilter.Controls.Add(this.txtFilterValue);
+            this.gbPersonFilter.Controls.Add(this.label1);
+            this.gbPersonFilter.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gbPersonFilter.Location = new System.Drawing.Point(3, 18);
+            this.gbPersonFilter.Name = "gbPersonFilter";
+            this.gbPersonFilter.Size = new System.Drawing.Size(601, 87);
+            this.gbPersonFilter.TabIndex = 1;
+            this.gbPersonFilter.TabStop = false;
+            this.gbPersonFilter.Text = "Person Filter";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find By :";
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtFilterValue.DefaultText = "";
+            this.txtFilterValue.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtFilterValue.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtFilterValue.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFilterValue.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFilterValue.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFilterValue.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtFilterValue.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFilterValue.Location = new System.Drawing.Point(281, 32);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.PlaceholderText = "";
+            this.txtFilterValue.SelectedText = "";
+            this.txtFilterValue.Size = new System.Drawing.Size(169, 27);
+            this.txtFilterValue.TabIndex = 2;
+            this.txtFilterValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilterValue_KeyPress);
+            this.txtFilterValue.Validating += new System.ComponentModel.CancelEventHandler(this.txtFilterValue_Validating);
+            // 
+            // btnAddNewPerson
+            // 
+            this.btnAddNewPerson.BorderRadius = 10;
+            this.btnAddNewPerson.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddNewPerson.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddNewPerson.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddNewPerson.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnAddNewPerson.FillColor = System.Drawing.Color.White;
+            this.btnAddNewPerson.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnAddNewPerson.ForeColor = System.Drawing.Color.White;
+            this.btnAddNewPerson.Image = global::DVLD.Properties.Resources.add_user;
+            this.btnAddNewPerson.ImageSize = new System.Drawing.Size(28, 28);
+            this.btnAddNewPerson.Location = new System.Drawing.Point(499, 24);
+            this.btnAddNewPerson.Name = "btnAddNewPerson";
+            this.btnAddNewPerson.Size = new System.Drawing.Size(35, 35);
+            this.btnAddNewPerson.TabIndex = 4;
+            this.btnAddNewPerson.Click += new System.EventHandler(this.btnAddNewPerson_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BorderRadius = 10;
+            this.btnSearch.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSearch.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSearch.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSearch.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSearch.FillColor = System.Drawing.Color.White;
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Image = global::DVLD.Properties.Resources.magnifying_glass;
+            this.btnSearch.ImageOffset = new System.Drawing.Point(1, 0);
+            this.btnSearch.ImageSize = new System.Drawing.Size(28, 28);
+            this.btnSearch.Location = new System.Drawing.Point(458, 23);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(35, 35);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cbFilterBy
+            // 
+            this.cbFilterBy.FormattingEnabled = true;
+            this.cbFilterBy.Items.AddRange(new object[] {
+            "NationalNo",
+            "PersonID"});
+            this.cbFilterBy.Location = new System.Drawing.Point(97, 32);
+            this.cbFilterBy.Name = "cbFilterBy";
+            this.cbFilterBy.Size = new System.Drawing.Size(178, 27);
+            this.cbFilterBy.TabIndex = 5;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // personUserControl11
+            // 
+            this.personUserControl11.Location = new System.Drawing.Point(3, 99);
+            this.personUserControl11.Name = "personUserControl11";
+            this.personUserControl11.Size = new System.Drawing.Size(647, 275);
+            this.personUserControl11.TabIndex = 0;
+            // 
+            // PersonInfoWithFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbPersonFilter);
+            this.Controls.Add(this.personUserControl11);
+            this.Name = "PersonInfoWithFilter";
+            this.Size = new System.Drawing.Size(649, 377);
+            this.Load += new System.EventHandler(this.PersonInfoWithFilter_Load);
+            this.gbPersonFilter.ResumeLayout(false);
+            this.gbPersonFilter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PersonUserControl1 personUserControl11;
+        private System.Windows.Forms.GroupBox gbPersonFilter;
+        private Guna.UI2.WinForms.Guna2Button btnAddNewPerson;
+        private Guna.UI2.WinForms.Guna2Button btnSearch;
+        private Guna.UI2.WinForms.Guna2TextBox txtFilterValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbFilterBy;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ DVLD/People/Controlls/PersonInfoWithFilter.cs
@@ -0,0 +1,184 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.People.Controlls
+{
+    public partial class PersonInfoWithFilter : UserControl
+    {
+
+        // Define a custom event handler delegate with parameters
+        public event Action<int> OnPersonSelected;
+        // Create a protected method to raise the event with a parameter
+        protected virtual void PersonSelected(int PersonID)
+        {
+            Action<int> handler = OnPersonSelected;
+            if (handler != null)
+            {
+                handler(PersonID); // Raise the event with the parameter
+            }
+        }
+
+        public PersonInfoWithFilter()
+        {
+            InitializeComponent();
+        }
+
+       
+
+
+
+        private bool _ShowAddPerson = true;
+
+        public bool ShowAddPerson
+        {
+            get
+            {
+                return _ShowAddPerson;
+            }
+            set
+            {
+                _ShowAddPerson = value;
+                btnAddNewPerson.Visible = _ShowAddPerson;
+            }
+
+        }
+
+        private bool _FilterEnabled = true;
+
+        public bool FilterEnabled
+        {
+            get { return _FilterEnabled; }
+            set
+            {
+                _FilterEnabled = value;
+                gbPersonFilter.Enabled = _FilterEnabled;
+            }
+
+        }
+
+        private int _PersonID = -1;
+
+        public int PersonID
+        {
+            get { return personUserControl11.PersonID; }
+            
+        }
+
+        public clsPerson SelectedPersonInfo
+        {
+            get { return personUserControl11.SelectedPersonInfo; }
+        }
+
+        public void LoadPersonInfo(int PersonID)
+        {
+            cbFilterBy.SelectedIndex = 1;
+            txtFilterValue.Text = PersonID.ToString();
+            FindNow();
+        }
+
+        private void FindNow()
+        {
+           switch(cbFilterBy.Text)
+            {
+                case "PersonID":
+                    personUserControl11.LoadPersonInfo(int.Parse(txtFilterValue.Text));
+
+                    break;
+
+                case "NationalNo":
+                    personUserControl11.LoadPersonInfo(txtFilterValue.Text);
+
+                    break;
+
+                default:
+                    break;
+
+
+                   
+                   
+            }
+
+            if (OnPersonSelected != null && FilterEnabled)
+
+                OnPersonSelected(personUserControl11.PersonID);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren())
+            {
+                //Here we dont continue becuase the form is not valid
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+
+            FindNow();
+        }
+
+        private void PersonInfoWithFilter_Load(object sender, EventArgs e)
+        {
+            cbFilterBy.SelectedIndex = 0;
+            txtFilterValue.Focus();
+        }
+
+        private void txtFilterValue_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFilterValue.Text.Trim()))
+            {
+                e.Cancel = true;
+               errorProvider1.SetError(txtFilterValue, "This field is required!");
+            }
+            else
+            {
+                //e.Cancel = false;
+                errorProvider1.SetError(txtFilterValue, null);
+            }
+            
+        }
+
+        private void btnAddNewPerson_Click(object sender, EventArgs e)
+        {
+            frmAddUpdatePerson frm = new frmAddUpdatePerson();
+            frm.DataBack += Frm_DataBack;
+            frm.ShowDialog();
+
+
+        }
+
+        private void Frm_DataBack(object sender, int PersonID)
+        {
+            cbFilterBy.SelectedIndex = 1;
+            txtFilterValue.Text = PersonID.ToString();
+            personUserControl11.LoadPersonInfo(PersonID);
+        }
+
+        public void FilterFocus()
+        {
+            txtFilterValue.Focus();
+        }
+
+        private void txtFilterValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Check if the pressed key is Enter (character code 13)
+            if (e.KeyChar == (char)13)
+            {
+
+                btnSearch.PerformClick();
+            }
+
+            //this will allow only digits if person id is selected
+            if (cbFilterBy.Text == "Person ID")
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+        }
+    }
+}
