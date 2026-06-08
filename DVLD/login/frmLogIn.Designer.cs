f112b37a40d538607eb3c4b07f311a974239dee2
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-08 11:10:33 +0100

    add login form

---
+++ DVLD/login/frmLogIn.Designer.cs
@@ -0,0 +1,276 @@
+﻿using System.Drawing;
+
+namespace DVLD
+{
+    partial class frmLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelLeft = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblTitel = new System.Windows.Forms.Label();
+            this.picLogo = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lblsubwelcome = new System.Windows.Forms.Label();
+            this.lblUserNameTitel = new System.Windows.Forms.Label();
+            this.lblPasswordTitel = new System.Windows.Forms.Label();
+            this.cbRememberMe = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.linklblForgetPass = new System.Windows.Forms.LinkLabel();
+            this.btnLogIn = new Guna.UI2.WinForms.Guna2Button();
+            this.txtUsername = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtPassword = new Guna.UI2.WinForms.Guna2TextBox();
+            this.exit = new System.Windows.Forms.Label();
+            this.PanelLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelLeft
+            // 
+            this.PanelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(112)))));
+            this.PanelLeft.Controls.Add(this.lblTitel);
+            this.PanelLeft.Controls.Add(this.picLogo);
+            this.PanelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelLeft.Location = new System.Drawing.Point(0, 0);
+            this.PanelLeft.Name = "PanelLeft";
+            this.PanelLeft.Size = new System.Drawing.Size(350, 450);
+            this.PanelLeft.TabIndex = 0;
+            // 
+            // lblTitel
+            // 
+            this.lblTitel.AutoSize = true;
+            this.lblTitel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitel.ForeColor = System.Drawing.Color.White;
+            this.lblTitel.Location = new System.Drawing.Point(35, 195);
+            this.lblTitel.Name = "lblTitel";
+            this.lblTitel.Size = new System.Drawing.Size(285, 30);
+            this.lblTitel.TabIndex = 1;
+            this.lblTitel.Text = "Driver License Management";
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = global::DVLD.Properties.Resources.dl_removebg_preview;
+            this.picLogo.ImageRotate = 0F;
+            this.picLogo.Location = new System.Drawing.Point(80, 12);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(180, 180);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(112)))));
+            this.lblWelcome.Location = new System.Drawing.Point(477, 12);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(217, 40);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "Welcome Back";
+            // 
+            // lblsubwelcome
+            // 
+            this.lblsubwelcome.AutoSize = true;
+            this.lblsubwelcome.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsubwelcome.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lblsubwelcome.Location = new System.Drawing.Point(503, 61);
+            this.lblsubwelcome.Name = "lblsubwelcome";
+            this.lblsubwelcome.Size = new System.Drawing.Size(161, 20);
+            this.lblsubwelcome.TabIndex = 2;
+            this.lblsubwelcome.Text = "Sign in to your account";
+            // 
+            // lblUserNameTitel
+            // 
+            this.lblUserNameTitel.AutoSize = true;
+            this.lblUserNameTitel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserNameTitel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(112)))));
+            this.lblUserNameTitel.Location = new System.Drawing.Point(394, 120);
+            this.lblUserNameTitel.Name = "lblUserNameTitel";
+            this.lblUserNameTitel.Size = new System.Drawing.Size(75, 17);
+            this.lblUserNameTitel.TabIndex = 3;
+            this.lblUserNameTitel.Text = "User Name";
+            // 
+            // lblPasswordTitel
+            // 
+            this.lblPasswordTitel.AutoSize = true;
+            this.lblPasswordTitel.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasswordTitel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(112)))));
+            this.lblPasswordTitel.Location = new System.Drawing.Point(394, 205);
+            this.lblPasswordTitel.Name = "lblPasswordTitel";
+            this.lblPasswordTitel.Size = new System.Drawing.Size(66, 17);
+            this.lblPasswordTitel.TabIndex = 4;
+            this.lblPasswordTitel.Text = "Password";
+            // 
+            // cbRememberMe
+            // 
+            this.cbRememberMe.AutoSize = true;
+            this.cbRememberMe.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbRememberMe.CheckedState.BorderRadius = 0;
+            this.cbRememberMe.CheckedState.BorderThickness = 0;
+            this.cbRememberMe.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbRememberMe.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbRememberMe.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(112)))));
+            this.cbRememberMe.Location = new System.Drawing.Point(397, 286);
+            this.cbRememberMe.Name = "cbRememberMe";
+            this.cbRememberMe.Size = new System.Drawing.Size(131, 24);
+            this.cbRememberMe.TabIndex = 6;
+            this.cbRememberMe.Text = " Remember me";
+            this.cbRememberMe.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.cbRememberMe.UncheckedState.BorderRadius = 0;
+            this.cbRememberMe.UncheckedState.BorderThickness = 0;
+            this.cbRememberMe.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            // 
+            // linklblForgetPass
+            // 
+            this.linklblForgetPass.AutoSize = true;
+            this.linklblForgetPass.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linklblForgetPass.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(212)))));
+            this.linklblForgetPass.Location = new System.Drawing.Point(648, 293);
+            this.linklblForgetPass.Name = "linklblForgetPass";
+            this.linklblForgetPass.Size = new System.Drawing.Size(120, 17);
+            this.linklblForgetPass.TabIndex = 8;
+            this.linklblForgetPass.TabStop = true;
+            this.linklblForgetPass.Text = "Forget Password ?";
+            this.linklblForgetPass.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linklblForgetPass_LinkClicked);
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnLogIn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnLogIn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnLogIn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnLogIn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(212)))));
+            this.btnLogIn.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogIn.ForeColor = System.Drawing.Color.White;
+            this.btnLogIn.Image = global::DVLD.Properties.Resources.login1;
+            this.btnLogIn.Location = new System.Drawing.Point(388, 337);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(380, 41);
+            this.btnLogIn.TabIndex = 12;
+            this.btnLogIn.Text = " Log In";
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderRadius = 10;
+            this.txtUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsername.DefaultText = "     Enter Your User Name";
+            this.txtUsername.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtUsername.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtUsername.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUsername.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUsername.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtUsername.IconLeft = global::DVLD.Properties.Resources.user__1_;
+            this.txtUsername.Location = new System.Drawing.Point(388, 140);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.PlaceholderText = "";
+            this.txtUsername.SelectedText = "";
+            this.txtUsername.Size = new System.Drawing.Size(381, 52);
+            this.txtUsername.TabIndex = 11;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderRadius = 10;
+            this.txtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPassword.DefaultText = "     Enter Your User Password";
+            this.txtPassword.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtPassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtPassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPassword.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPassword.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPassword.IconLeft = global::DVLD.Properties.Resources.locked_computer;
+            this.txtPassword.IconRight = global::DVLD.Properties.Resources.visible;
+            this.txtPassword.Location = new System.Drawing.Point(388, 228);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PlaceholderText = "";
+            this.txtPassword.SelectedText = "";
+            this.txtPassword.Size = new System.Drawing.Size(381, 52);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // exit
+            // 
+            this.exit.AutoSize = true;
+            this.exit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.exit.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(112)))));
+            this.exit.Location = new System.Drawing.Point(775, 0);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(36, 40);
+            this.exit.TabIndex = 13;
+            this.exit.Text = "X";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // frmLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 450);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.linklblForgetPass);
+            this.Controls.Add(this.cbRememberMe);
+            this.Controls.Add(this.lblPasswordTitel);
+            this.Controls.Add(this.lblUserNameTitel);
+            this.Controls.Add(this.lblsubwelcome);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.PanelLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "frmLogIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Log In";
+            this.Load += new System.EventHandler(this.frmLogIn_Load);
+            this.PanelLeft.ResumeLayout(false);
+            this.PanelLeft.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel PanelLeft;
+        private Guna.UI2.WinForms.Guna2PictureBox picLogo;
+        private System.Windows.Forms.Label lblTitel;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label lblsubwelcome;
+        private System.Windows.Forms.Label lblUserNameTitel;
+        private System.Windows.Forms.Label lblPasswordTitel;
+        private Guna.UI2.WinForms.Guna2CheckBox cbRememberMe;
+        private System.Windows.Forms.LinkLabel linklblForgetPass;
+        private Guna.UI2.WinForms.Guna2TextBox txtPassword;
+        private Guna.UI2.WinForms.Guna2TextBox txtUsername;
+        private Guna.UI2.WinForms.Guna2Button btnLogIn;
+        private System.Windows.Forms.Label exit;
+    }
+}
---
+++ DVLD/login/frmLogIn.cs
@@ -0,0 +1,96 @@
+﻿using DVLD.Globle_Classes;
+using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD
+{
+    public partial class frmLogIn : Form
+    {
+        public frmLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void linklblForgetPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("This feature is Not Implemented Yet! ", "Not Ready", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+          clsUser user = clsUser.FindByUserNameAndPassword(txtUsername.Text.Trim(), txtPassword.Text.Trim());  
+
+            if(user != null )
+            {
+
+                if (cbRememberMe.Checked)
+                {
+                    //store username and password
+                    GlobalClass.RememberUsernameAndPassword(txtUsername.Text.Trim(), txtPassword.Text.Trim());
+                }
+                else
+                {
+                    //store empty username and password
+                    GlobalClass.RememberUsernameAndPassword("", "");
+                }
+
+                //incase the user is not active
+                if (!user.IsActive)
+                {
+
+                    txtUsername.Focus();
+                    MessageBox.Show("Your accound is not Active, Contact Admin.", "In Active Account", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                GlobalClass.CurrentUser = user;
+                this.Hide();
+                frmDashbord frm = new frmDashbord(this);
+                frm.ShowDialog();
+
+            }
+            else
+            {
+                txtUsername.Focus();
+                MessageBox.Show("Invalid Username/Password! ", "Wrong Credential",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void frmLogIn_Load(object sender, EventArgs e)
+        {
+            string username = "", password = "";
+
+            if(GlobalClass.GetStoredCredential(ref username, ref password ))
+            {
+                txtUsername.Text = username;
+                txtPassword.Text = password;
+                cbRememberMe.Checked = true;
+
+            }
+            else
+            {
+                cbRememberMe.Checked= false;
+            }
+
+
+
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
