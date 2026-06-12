4b5f32249b8e6b70b6fb1f5521f033e82f6bd54d
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-12 09:36:29 +0100

    add list of application Types

---
+++ DVLD/Applications/ApplicationType/frmManageApplicationType.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DVLD.Application.ApplicationType
+{
+    partial class frmManageApplicationType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitel = new System.Windows.Forms.Label();
+            this.dgvApplicationtypes = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editApplicationTypeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnclose = new Guna.UI2.WinForms.Guna2Button();
+            this.lblRecordsCountapptypes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvApplicationtypes)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitel
+            // 
+            this.lblTitel.AutoSize = true;
+            this.lblTitel.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lblTitel.ForeColor = System.Drawing.Color.Red;
+            this.lblTitel.Location = new System.Drawing.Point(219, 146);
+            this.lblTitel.Name = "lblTitel";
+            this.lblTitel.Size = new System.Drawing.Size(380, 36);
+            this.lblTitel.TabIndex = 12;
+            this.lblTitel.Text = "Manage Application Types";
+            // 
+            // dgvApplicationtypes
+            // 
+            this.dgvApplicationtypes.AllowUserToAddRows = false;
+            this.dgvApplicationtypes.AllowUserToDeleteRows = false;
+            this.dgvApplicationtypes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvApplicationtypes.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgvApplicationtypes.Location = new System.Drawing.Point(73, 185);
+            this.dgvApplicationtypes.Name = "dgvApplicationtypes";
+            this.dgvApplicationtypes.ReadOnly = true;
+            this.dgvApplicationtypes.Size = new System.Drawing.Size(661, 300);
+            this.dgvApplicationtypes.TabIndex = 17;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editApplicationTypeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(257, 34);
+            // 
+            // editApplicationTypeToolStripMenuItem
+            // 
+            this.editApplicationTypeToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.editApplicationTypeToolStripMenuItem.Name = "editApplicationTypeToolStripMenuItem";
+            this.editApplicationTypeToolStripMenuItem.Size = new System.Drawing.Size(256, 30);
+            this.editApplicationTypeToolStripMenuItem.Text = "Edit Application Type";
+            // 
+            // btnclose
+            // 
+            this.btnclose.BorderRadius = 10;
+            this.btnclose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnclose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnclose.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnclose.Location = new System.Drawing.Point(649, 512);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(155, 53);
+            this.btnclose.TabIndex = 22;
+            this.btnclose.Text = "Close";
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // lblRecordsCountapptypes
+            // 
+            this.lblRecordsCountapptypes.AutoSize = true;
+            this.lblRecordsCountapptypes.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordsCountapptypes.Location = new System.Drawing.Point(127, 512);
+            this.lblRecordsCountapptypes.Name = "lblRecordsCountapptypes";
+            this.lblRecordsCountapptypes.Size = new System.Drawing.Size(17, 19);
+            this.lblRecordsCountapptypes.TabIndex = 21;
+            this.lblRecordsCountapptypes.Text = "?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 512);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 19);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "#Records: ";
+            // 
+            // guna2CirclePictureBox1
+            // 
+            this.guna2CirclePictureBox1.Image = global::DVLD.Properties.Resources.apptypes_removebg_preview;
+            this.guna2CirclePictureBox1.ImageRotate = 0F;
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(310, 12);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(222, 131);
+            this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox1.TabIndex = 23;
+            this.guna2CirclePictureBox1.TabStop = false;
+            // 
+            // frmManageApplicationType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(833, 566);
+            this.Controls.Add(this.guna2CirclePictureBox1);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.lblRecordsCountapptypes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvApplicationtypes);
+            this.Controls.Add(this.lblTitel);
+            this.Name = "frmManageApplicationType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmApplicationType";
+            this.Load += new System.EventHandler(this.frmManageApplicationType_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvApplicationtypes)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitel;
+        private System.Windows.Forms.DataGridView dgvApplicationtypes;
+        private Guna.UI2.WinForms.Guna2Button btnclose;
+        private System.Windows.Forms.Label lblRecordsCountapptypes;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editApplicationTypeToolStripMenuItem;
+    }
+}
---
+++ DVLD/Applications/ApplicationType/frmManageApplicationType.cs
@@ -0,0 +1,60 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Application.ApplicationType
+{
+    public partial class frmManageApplicationType : Form
+    {
+
+        private DataTable _dtApplicationType = clsApplicationType.GetAllApplicationTypes();
+
+        public frmManageApplicationType()
+        {
+            InitializeComponent();
+        }
+
+        private void frmManageApplicationType_Load(object sender, EventArgs e)
+        {
+            _RefreschAplicationTypesList();
+
+            if(dgvApplicationtypes.Rows.Count > 0 )
+            {
+                dgvApplicationtypes.Columns[0].HeaderText = "ID";
+                dgvApplicationtypes.Columns[0].Width = 110;
+
+                dgvApplicationtypes.Columns[1].HeaderText = "Titel";
+                dgvApplicationtypes.Columns[1].Width = 400;
+
+                dgvApplicationtypes.Columns[2].HeaderText = "Fees";
+                dgvApplicationtypes.Columns[2].Width = 100;
+
+            }
+
+        }
+
+        private void _RefreschAplicationTypesList()
+        {
+            dgvApplicationtypes.DataSource = _dtApplicationType;
+            _RefreschCount();
+        }
+
+        private void _RefreschCount()
+        {
+            int Record = dgvApplicationtypes.RowCount;
+            lblRecordsCountapptypes.Text = Record.ToString();
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
