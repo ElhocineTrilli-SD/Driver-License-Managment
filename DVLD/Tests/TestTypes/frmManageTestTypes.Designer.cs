1a951dbab41c492cad09f68fd421454db5bc8660
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-13 09:14:22 +0100

    implemenrted TestTypes List

---
+++ DVLD/Tests/TestTypes/frmManageTestTypes.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DVLD.Tests.TestTypes
+{
+    partial class frmManageTestTypes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnclose = new Guna.UI2.WinForms.Guna2Button();
+            this.lblRecordsCountTestTypes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvTestTypes = new System.Windows.Forms.DataGridView();
+            this.lblTitel = new System.Windows.Forms.Label();
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editTestTypeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTestTypes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.BorderRadius = 10;
+            this.btnclose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnclose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnclose.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnclose.Location = new System.Drawing.Point(674, 492);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(155, 53);
+            this.btnclose.TabIndex = 28;
+            this.btnclose.Text = "Close";
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // lblRecordsCountTestTypes
+            // 
+            this.lblRecordsCountTestTypes.AutoSize = true;
+            this.lblRecordsCountTestTypes.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordsCountTestTypes.Location = new System.Drawing.Point(123, 492);
+            this.lblRecordsCountTestTypes.Name = "lblRecordsCountTestTypes";
+            this.lblRecordsCountTestTypes.Size = new System.Drawing.Size(17, 19);
+            this.lblRecordsCountTestTypes.TabIndex = 27;
+            this.lblRecordsCountTestTypes.Text = "?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 492);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 19);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "#Records: ";
+            // 
+            // dgvTestTypes
+            // 
+            this.dgvTestTypes.AllowUserToAddRows = false;
+            this.dgvTestTypes.AllowUserToDeleteRows = false;
+            this.dgvTestTypes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTestTypes.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgvTestTypes.Location = new System.Drawing.Point(80, 186);
+            this.dgvTestTypes.Name = "dgvTestTypes";
+            this.dgvTestTypes.ReadOnly = true;
+            this.dgvTestTypes.Size = new System.Drawing.Size(661, 300);
+            this.dgvTestTypes.TabIndex = 25;
+            // 
+            // lblTitel
+            // 
+            this.lblTitel.AutoSize = true;
+            this.lblTitel.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lblTitel.ForeColor = System.Drawing.Color.Red;
+            this.lblTitel.Location = new System.Drawing.Point(266, 146);
+            this.lblTitel.Name = "lblTitel";
+            this.lblTitel.Size = new System.Drawing.Size(280, 36);
+            this.lblTitel.TabIndex = 24;
+            this.lblTitel.Text = "Manage Test Types";
+            // 
+            // guna2CirclePictureBox1
+            // 
+            this.guna2CirclePictureBox1.Image = global::DVLD.Properties.Resources.apptypes_removebg_preview;
+            this.guna2CirclePictureBox1.ImageRotate = 0F;
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(299, 12);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(222, 131);
+            this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox1.TabIndex = 29;
+            this.guna2CirclePictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editTestTypeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(193, 34);
+            // 
+            // editTestTypeToolStripMenuItem
+            // 
+            this.editTestTypeToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.editTestTypeToolStripMenuItem.Name = "editTestTypeToolStripMenuItem";
+            this.editTestTypeToolStripMenuItem.Size = new System.Drawing.Size(192, 30);
+            this.editTestTypeToolStripMenuItem.Text = "Edit Test Type";
+            // 
+            // frmManageTestTypes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(832, 554);
+            this.Controls.Add(this.guna2CirclePictureBox1);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.lblRecordsCountTestTypes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvTestTypes);
+            this.Controls.Add(this.lblTitel);
+            this.Name = "frmManageTestTypes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmManageTestTypes";
+            this.Load += new System.EventHandler(this.frmManageTestTypes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTestTypes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnclose;
+        private System.Windows.Forms.Label lblRecordsCountTestTypes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvTestTypes;
+        private System.Windows.Forms.Label lblTitel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editTestTypeToolStripMenuItem;
+    }
+}
---
+++ DVLD/Tests/TestTypes/frmManageTestTypes.cs
@@ -0,0 +1,63 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Tests.TestTypes
+{
+    public partial class frmManageTestTypes : Form
+    {
+
+        private DataTable _DtTests;
+
+        public frmManageTestTypes()
+        {
+            InitializeComponent();
+        }
+      
+        private void frmManageTestTypes_Load(object sender, EventArgs e)
+        {
+            _RefreschTestTypesList();
+
+            if (dgvTestTypes.Rows.Count > 0)
+            {
+                dgvTestTypes.Columns[0].HeaderText = "ID";
+                dgvTestTypes.Columns[0].Width = 110;
+
+                dgvTestTypes.Columns[1].HeaderText = "Titel";
+                dgvTestTypes.Columns[1].Width = 200;
+
+                dgvTestTypes.Columns[2].HeaderText = "Description";
+                dgvTestTypes.Columns[2].Width = 400;
+
+                dgvTestTypes.Columns[3].HeaderText = "Fees";
+                dgvTestTypes.Columns[3].Width = 100;
+            }
+        }
+
+        private void _RefreschTestTypesList()
+        {
+           _DtTests = clsTestType.GetAllTestTypes();
+            dgvTestTypes.DataSource = _DtTests;
+            _RefreschCount();
+        }
+
+       private void _RefreschCount()
+        {
+            int Record = dgvTestTypes.RowCount;
+            lblRecordsCountTestTypes.Text = Record.ToString();
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    
+}
