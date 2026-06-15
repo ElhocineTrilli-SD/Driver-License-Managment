e29ef02ac67b7c6c4b670fae9db749a18436cb15
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-15 15:53:54 +0100

    add form to show details

---
+++ DVLD/Applications/Local Driving License/frmLocalDrivingLicenseApplicationInfo.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace DVLD.Applications.Local_Driving_License
+{
+    partial class frmLocalDrivingLicenseApplicationInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.ctrlDrivingLicenseApplication1 = new DVLD.Applications.Local_Driving_License.ctrlDrivingLicenseApplication();
+            this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.Location = new System.Drawing.Point(620, 409);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(148, 44);
+            this.guna2Button1.TabIndex = 4;
+            this.guna2Button1.Text = "Close";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // ctrlDrivingLicenseApplication1
+            // 
+            this.ctrlDrivingLicenseApplication1.Location = new System.Drawing.Point(12, 12);
+            this.ctrlDrivingLicenseApplication1.Name = "ctrlDrivingLicenseApplication1";
+            this.ctrlDrivingLicenseApplication1.Size = new System.Drawing.Size(785, 389);
+            this.ctrlDrivingLicenseApplication1.TabIndex = 5;
+            // 
+            // frmLocalDrivingLicenseApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(809, 465);
+            this.Controls.Add(this.ctrlDrivingLicenseApplication1);
+            this.Controls.Add(this.guna2Button1);
+            this.Name = "frmLocalDrivingLicenseApplication";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Local Driving License Application";
+            this.Load += new System.EventHandler(this.frmLocalDrivingLicenseApplication_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private ctrlDrivingLicenseApplication ctrlDrivingLicenseApplication1;
+    }
+}
---
+++ DVLD/Applications/Local Driving License/frmLocalDrivingLicenseApplicationInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Applications.Local_Driving_License
+{
+    public partial class frmLocalDrivingLicenseApplicationInfo : Form
+    {
+        public int _ApplicationID = -1;
+
+
+        public frmLocalDrivingLicenseApplicationInfo(int applicationID)
+        {
+            InitializeComponent();
+            _ApplicationID = applicationID;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmLocalDrivingLicenseApplication_Load(object sender, EventArgs e)
+        {
+            ctrlDrivingLicenseApplication1.LoadApplicationInfoByApplicationID(_ApplicationID);
+        }
+    }
+}
