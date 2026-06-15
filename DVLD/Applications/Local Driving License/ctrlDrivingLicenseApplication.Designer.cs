231472ab189c733162b4dc9514dea33de6365459
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-15 12:05:48 +0100

    add control to show application Basic info and Loclal driving license info

---
+++ DVLD/Applications/Local Driving License/ctrlDrivingLicenseApplication.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace DVLD.Applications.Local_Driving_License
+{
+    partial class ctrlDrivingLicenseApplication
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grDrivingLicenseA = new System.Windows.Forms.GroupBox();
+            this.txtTest = new System.Windows.Forms.Label();
+            this.txtAppliedforL = new System.Windows.Forms.Label();
+            this.txtDLApplicationID = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.userControl_ApplicationBasic1 = new DVLD.Applications.controlls.UserControl_ApplicationBasic();
+            this.grDrivingLicenseA.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grDrivingLicenseA
+            // 
+            this.grDrivingLicenseA.Controls.Add(this.txtTest);
+            this.grDrivingLicenseA.Controls.Add(this.txtAppliedforL);
+            this.grDrivingLicenseA.Controls.Add(this.txtDLApplicationID);
+            this.grDrivingLicenseA.Controls.Add(this.linkLabel1);
+            this.grDrivingLicenseA.Controls.Add(this.label2);
+            this.grDrivingLicenseA.Controls.Add(this.label1);
+            this.grDrivingLicenseA.Controls.Add(this.lblUserID);
+            this.grDrivingLicenseA.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.grDrivingLicenseA.Location = new System.Drawing.Point(3, 3);
+            this.grDrivingLicenseA.Name = "grDrivingLicenseA";
+            this.grDrivingLicenseA.Size = new System.Drawing.Size(770, 131);
+            this.grDrivingLicenseA.TabIndex = 0;
+            this.grDrivingLicenseA.TabStop = false;
+            this.grDrivingLicenseA.Text = " Driving License Application Info";
+            this.grDrivingLicenseA.Enter += new System.EventHandler(this.grDrivingLicenseA_Enter);
+            // 
+            // txtTest
+            // 
+            this.txtTest.AutoSize = true;
+            this.txtTest.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtTest.ForeColor = System.Drawing.Color.Black;
+            this.txtTest.Location = new System.Drawing.Point(480, 78);
+            this.txtTest.Name = "txtTest";
+            this.txtTest.Size = new System.Drawing.Size(34, 19);
+            this.txtTest.TabIndex = 54;
+            this.txtTest.Text = "0/0";
+            // 
+            // txtAppliedforL
+            // 
+            this.txtAppliedforL.AutoSize = true;
+            this.txtAppliedforL.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtAppliedforL.ForeColor = System.Drawing.Color.Black;
+            this.txtAppliedforL.Location = new System.Drawing.Point(529, 40);
+            this.txtAppliedforL.Name = "txtAppliedforL";
+            this.txtAppliedforL.Size = new System.Drawing.Size(23, 19);
+            this.txtAppliedforL.TabIndex = 53;
+            this.txtAppliedforL.Text = "??";
+            // 
+            // txtDLApplicationID
+            // 
+            this.txtDLApplicationID.AutoSize = true;
+            this.txtDLApplicationID.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtDLApplicationID.ForeColor = System.Drawing.Color.Red;
+            this.txtDLApplicationID.Location = new System.Drawing.Point(173, 40);
+            this.txtDLApplicationID.Name = "txtDLApplicationID";
+            this.txtDLApplicationID.Size = new System.Drawing.Size(23, 19);
+            this.txtDLApplicationID.TabIndex = 52;
+            this.txtDLApplicationID.Text = "??";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.linkLabel1.Location = new System.Drawing.Point(64, 78);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(145, 19);
+            this.linkLabel1.TabIndex = 51;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Show License Info";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(363, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 19);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "Passed Tests :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(363, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 19);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Applied for License :";
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblUserID.Location = new System.Drawing.Point(15, 40);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(152, 19);
+            this.lblUserID.TabIndex = 48;
+            this.lblUserID.Text = "D.L.Application ID :";
+            // 
+            // userControl_ApplicationBasic1
+            // 
+            this.userControl_ApplicationBasic1.Location = new System.Drawing.Point(3, 141);
+            this.userControl_ApplicationBasic1.Name = "userControl_ApplicationBasic1";
+            this.userControl_ApplicationBasic1.Size = new System.Drawing.Size(790, 248);
+            this.userControl_ApplicationBasic1.TabIndex = 1;
+            this.userControl_ApplicationBasic1.Load += new System.EventHandler(this.userControl_ApplicationBasic1_Load);
+            // 
+            // ctrlDrivingLicenseApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.userControl_ApplicationBasic1);
+            this.Controls.Add(this.grDrivingLicenseA);
+            this.Name = "ctrlDrivingLicenseApplication";
+            this.Size = new System.Drawing.Size(785, 389);
+            this.Load += new System.EventHandler(this.ctrlDrivingLicenseApplication_Load);
+            this.grDrivingLicenseA.ResumeLayout(false);
+            this.grDrivingLicenseA.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grDrivingLicenseA;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblUserID;
+        private System.Windows.Forms.Label txtTest;
+        private System.Windows.Forms.Label txtAppliedforL;
+        private System.Windows.Forms.Label txtDLApplicationID;
+        private controlls.UserControl_ApplicationBasic userControl_ApplicationBasic1;
+    }
+}
---
+++ DVLD/Applications/Local Driving License/ctrlDrivingLicenseApplication.cs
@@ -0,0 +1,101 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Applications.Local_Driving_License
+{
+    public partial class ctrlDrivingLicenseApplication : UserControl
+    {
+        private clsLocalDrivingLicenseApplication _LocalDrivingLicenseApplication;
+
+        private int _LocalDrivingLicenseApplicationID = -1;
+
+        private int _LicenseID;
+
+        public int LocalDrivingLicenseApplicationID
+        {
+            get { return _LocalDrivingLicenseApplicationID; }
+        }
+
+        public ctrlDrivingLicenseApplication()
+        {
+            InitializeComponent();
+        }
+        public void LoadApplicationInfoByLocalDrivingAppID(int LocalDrivingLicenseApplicationID)
+        {
+            _LocalDrivingLicenseApplication = clsLocalDrivingLicenseApplication.FindByLocalDrivingAppLicenseID(LocalDrivingLicenseApplicationID);
+            if (_LocalDrivingLicenseApplication == null)
+            {
+                _ResetLocalDrivingLicenseApplicationInfo();
+
+
+                MessageBox.Show("No Application with ApplicationID = " + LocalDrivingLicenseApplicationID.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _FillLocalDrivingLicenseApplicationInfo();
+        }
+
+        public void LoadApplicationInfoByApplicationID(int ApplicationID)
+        {
+            _LocalDrivingLicenseApplication = clsLocalDrivingLicenseApplication.FindByApplicationID(ApplicationID);
+            if (_LocalDrivingLicenseApplication == null)
+            {
+                _ResetLocalDrivingLicenseApplicationInfo();
+
+
+                MessageBox.Show("No Application with ApplicationID = " + LocalDrivingLicenseApplicationID.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _FillLocalDrivingLicenseApplicationInfo();
+        }
+
+        private void _ResetLocalDrivingLicenseApplicationInfo()
+        {
+            _LocalDrivingLicenseApplicationID = -1;
+           
+            txtDLApplicationID.Text = "??";
+            txtAppliedforL.Text = "??";
+
+
+        }
+
+        private void _FillLocalDrivingLicenseApplicationInfo()
+        {
+           // _LicenseID = _LocalDrivingLicenseApplication.GetActiveLicenseID();
+
+            //incase there is license enable the show link.
+          //  llShowLicenceInfo.Enabled = (_LicenseID != -1);
+
+
+            txtDLApplicationID.Text = _LocalDrivingLicenseApplication.LocalDrivingLicenseApplicationID.ToString();
+            txtAppliedforL.Text = clsLicenseClass.Find(_LocalDrivingLicenseApplication.LicenseClassID).ClassName;
+           // lblPassedTests.Text = _LocalDrivingLicenseApplication.GetPassedTestCount().ToString() + "/3";
+            userControl_ApplicationBasic1.LoadApplicationInfo(_LocalDrivingLicenseApplication.ApplicationID);
+
+        }
+
+        private void ctrlDrivingLicenseApplication_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grDrivingLicenseA_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userControl_ApplicationBasic1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
