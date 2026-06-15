31964bd7e5dac6297bcb37392f0dc1bca51af93c
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-15 15:53:28 +0100

    add manage Local License Form

---
+++ DVLD/Applications/Local Driving License/frmListlocalDrivingLicenseApplication.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace DVLD.Applications.Local_Driving_License
+{
+    partial class frmListlocalDrivingLicenseApplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnclose = new Guna.UI2.WinForms.Guna2Button();
+            this.lblRecordsCountLD = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvLD = new System.Windows.Forms.DataGridView();
+            this.txtFilterValue = new System.Windows.Forms.TextBox();
+            this.cbfilterLD = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.pbUsers = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showApplicationDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUsers)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.BorderRadius = 10;
+            this.btnclose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnclose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnclose.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnclose.Location = new System.Drawing.Point(629, 509);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(155, 53);
+            this.btnclose.TabIndex = 30;
+            this.btnclose.Text = "Close";
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // lblRecordsCountLD
+            // 
+            this.lblRecordsCountLD.AutoSize = true;
+            this.lblRecordsCountLD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordsCountLD.Location = new System.Drawing.Point(107, 509);
+            this.lblRecordsCountLD.Name = "lblRecordsCountLD";
+            this.lblRecordsCountLD.Size = new System.Drawing.Size(17, 19);
+            this.lblRecordsCountLD.TabIndex = 29;
+            this.lblRecordsCountLD.Text = "?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 509);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 19);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "#Records: ";
+            // 
+            // dgvLD
+            // 
+            this.dgvLD.AllowUserToAddRows = false;
+            this.dgvLD.AllowUserToDeleteRows = false;
+            this.dgvLD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLD.Location = new System.Drawing.Point(23, 200);
+            this.dgvLD.Name = "dgvLD";
+            this.dgvLD.ReadOnly = true;
+            this.dgvLD.Size = new System.Drawing.Size(761, 291);
+            this.dgvLD.TabIndex = 27;
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.Location = new System.Drawing.Point(273, 172);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.Size = new System.Drawing.Size(137, 20);
+            this.txtFilterValue.TabIndex = 25;
+            this.txtFilterValue.Visible = false;
+            this.txtFilterValue.TextChanged += new System.EventHandler(this.txtFilterValue_TextChanged);
+            this.txtFilterValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilterValue_KeyPress);
+            // 
+            // cbfilterLD
+            // 
+            this.cbfilterLD.FormattingEnabled = true;
+            this.cbfilterLD.Location = new System.Drawing.Point(106, 172);
+            this.cbfilterLD.Name = "cbfilterLD";
+            this.cbfilterLD.Size = new System.Drawing.Size(161, 21);
+            this.cbfilterLD.TabIndex = 24;
+            this.cbfilterLD.SelectedIndexChanged += new System.EventHandler(this.cbfilterLD_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 22);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Filter By: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(155, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(480, 36);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Local Driving License Applications";
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnAddUser.BackgroundImage = global::DVLD.Properties.Resources.addpeople;
+            this.btnAddUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAddUser.Location = new System.Drawing.Point(713, 128);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(71, 66);
+            this.btnAddUser.TabIndex = 23;
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
+            // 
+            // pbUsers
+            // 
+            this.pbUsers.Image = global::DVLD.Properties.Resources.open_enrollment__1_;
+            this.pbUsers.ImageRotate = 0F;
+            this.pbUsers.Location = new System.Drawing.Point(312, 12);
+            this.pbUsers.Name = "pbUsers";
+            this.pbUsers.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.pbUsers.Size = new System.Drawing.Size(184, 113);
+            this.pbUsers.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbUsers.TabIndex = 21;
+            this.pbUsers.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showApplicationDetailsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(205, 48);
+            // 
+            // showApplicationDetailsToolStripMenuItem
+            // 
+            this.showApplicationDetailsToolStripMenuItem.Name = "showApplicationDetailsToolStripMenuItem";
+            this.showApplicationDetailsToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.showApplicationDetailsToolStripMenuItem.Text = "Show Application details";
+            // 
+            // frmListlocalDrivingLicenseApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(809, 568);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.lblRecordsCountLD);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvLD);
+            this.Controls.Add(this.txtFilterValue);
+            this.Controls.Add(this.cbfilterLD);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbUsers);
+            this.Name = "frmListlocalDrivingLicenseApplication";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "List local Driving License Application";
+            this.Load += new System.EventHandler(this.frmListlocalDrivingLicenseApplication_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUsers)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Button btnclose;
+        private System.Windows.Forms.Label lblRecordsCountLD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvLD;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.ComboBox cbfilterLD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbUsers;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showApplicationDetailsToolStripMenuItem;
+    }
+}
---
+++ DVLD/Applications/Local Driving License/frmListlocalDrivingLicenseApplication.cs
@@ -0,0 +1,174 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DVLD.Applications.Local_Driving_License
+{
+    public partial class frmListlocalDrivingLicenseApplication : Form
+    {
+        public delegate void handler(object sender, int records);
+
+        // Declare an event using the delegate
+        public event handler DataBack;
+
+
+        private DataTable _LDA = clsLocalDrivingLicenseApplication.GetAllLocalDrivingLicenseApplications();
+
+        public frmListlocalDrivingLicenseApplication()
+        {
+            InitializeComponent();
+        }
+        private void _FillcbfilteringwithItems()
+        {
+           cbfilterLD.Items.Add("None");
+           cbfilterLD.Items.Add("L.D.L AppID");
+           cbfilterLD.Items.Add("NationalNO");
+          
+           cbfilterLD.Items.Add("Full Name");
+           cbfilterLD.Items.Add("Status");
+
+           cbfilterLD.SelectedIndex = 0;
+
+        }
+
+        public void _RefrechUserList()
+        {
+            dgvLD.DataSource = _LDA ;
+            _RefrechRecordCount();
+        }
+        public void _RefrechRecordCount()
+        {
+            int Records = dgvLD.RowCount;
+            lblRecordsCountLD.Text = Records.ToString();
+
+
+            DataBack?.Invoke(this, Records);
+        }
+        private void frmListlocalDrivingLicenseApplication_Load(object sender, EventArgs e)
+        {
+            _RefrechUserList();
+            _FillcbfilteringwithItems();
+
+            if(_LDA.Rows.Count > 0)
+            {
+                dgvLD.Columns[0].HeaderText = "D.L.AppID";
+                dgvLD.Columns[0].Width = 110;
+
+                dgvLD.Columns[1].HeaderText = "Driving Class";
+                dgvLD.Columns[1].Width = 220;
+
+                dgvLD.Columns[2].HeaderText = "NationalNO";
+                dgvLD.Columns[2].Width = 110;
+
+                dgvLD.Columns[3].HeaderText = "Full Name";
+                dgvLD.Columns[3].Width = 250;
+
+                dgvLD.Columns[4].HeaderText = "Application Date";
+                dgvLD.Columns[4].Width = 160;
+
+                dgvLD.Columns[4].HeaderText = "Passed Tests";
+                dgvLD.Columns[4].Width = 160;
+
+                dgvLD.Columns[4].HeaderText = "Status";
+                dgvLD.Columns[4].Width = 120;
+
+
+            }
+
+        }
+
+        private void txtFilterValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (cbfilterLD.Text == "L.D.L AppID" )
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            frmAdd_EditLocalDrivingLicenseApplication frm = new frmAdd_EditLocalDrivingLicenseApplication();
+            frm.ShowDialog();
+        }
+
+        private void txtFilterValue_TextChanged(object sender, EventArgs e)
+        {
+            string FilterColumn = "";
+            //Map Selected Filter to real Column name 
+            switch (cbfilterLD.Text)
+            {
+                case "L.D.L AppID":
+                    FilterColumn = "LocalDrivingLicenseApplicationID";
+                    break;
+                case "NationalNO":
+                    FilterColumn = "NationalNo";
+                    break;
+
+                case "Full Name":
+                    FilterColumn = "FullName";
+                    break;
+
+
+                case "Status":
+                    FilterColumn = "Status";
+                    break;
+
+                default:
+                    FilterColumn = "None";
+                    break;
+
+            }
+
+            //Reset the filters in case nothing selected or filter value conains nothing.
+            if (txtFilterValue.Text.Trim() == "" || FilterColumn == "None")
+            {
+                _LDA.DefaultView.RowFilter = "";
+                lblRecordsCountLD.Text = _LDA.Rows.Count.ToString();
+                return;
+            }
+
+            DataColumn col = _LDA.Columns[FilterColumn];
+
+            if (col.DataType == typeof(string))
+            {
+                _LDA.DefaultView.RowFilter =
+                    $"[{FilterColumn}] LIKE '{txtFilterValue.Text.Trim()}%'";
+            }
+            else
+            {
+                _LDA.DefaultView.RowFilter =
+                    $"[{FilterColumn}] = {txtFilterValue.Text.Trim()}";
+            }
+
+        }
+
+        private void cbfilterLD_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            txtFilterValue.Visible = (cbfilterLD.SelectedIndex != 0);
+
+            if(cbfilterLD.SelectedIndex == 0)
+            {
+                txtFilterValue.Enabled = false;
+            }
+            else
+            {
+                txtFilterValue.Enabled= true;
+                txtFilterValue.Text = "";
+                txtFilterValue.Focus();
+            }
+
+        }
+    }
+}
