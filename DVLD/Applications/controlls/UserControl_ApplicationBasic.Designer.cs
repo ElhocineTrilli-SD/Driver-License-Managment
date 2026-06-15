9212786d8724a14a8a0bd1b51363e2574cd1de70
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-15 12:03:51 +0100

    add application basic info control

---
+++ DVLD/Applications/controlls/UserControl_ApplicationBasic.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace DVLD.Applications.controlls
+{
+    partial class UserControl_ApplicationBasic
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grA = new System.Windows.Forms.GroupBox();
+            this.llShowPersonInfo = new System.Windows.Forms.LinkLabel();
+            this.txtDate = new System.Windows.Forms.Label();
+            this.txtStatusDate = new System.Windows.Forms.Label();
+            this.txtCreatedBy = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.Label();
+            this.txtFees = new System.Windows.Forms.Label();
+            this.txtType = new System.Windows.Forms.Label();
+            this.txtApplicant = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grA.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grA
+            // 
+            this.grA.Controls.Add(this.llShowPersonInfo);
+            this.grA.Controls.Add(this.txtDate);
+            this.grA.Controls.Add(this.txtStatusDate);
+            this.grA.Controls.Add(this.txtCreatedBy);
+            this.grA.Controls.Add(this.label10);
+            this.grA.Controls.Add(this.label9);
+            this.grA.Controls.Add(this.label8);
+            this.grA.Controls.Add(this.txtStatus);
+            this.grA.Controls.Add(this.txtFees);
+            this.grA.Controls.Add(this.txtType);
+            this.grA.Controls.Add(this.txtApplicant);
+            this.grA.Controls.Add(this.txtID);
+            this.grA.Controls.Add(this.label7);
+            this.grA.Controls.Add(this.label6);
+            this.grA.Controls.Add(this.label5);
+            this.grA.Controls.Add(this.label4);
+            this.grA.Controls.Add(this.label3);
+            this.grA.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.grA.Location = new System.Drawing.Point(3, 3);
+            this.grA.Name = "grA";
+            this.grA.Size = new System.Drawing.Size(770, 233);
+            this.grA.TabIndex = 56;
+            this.grA.TabStop = false;
+            this.grA.Text = "Application Basic Info";
+            // 
+            // llShowPersonInfo
+            // 
+            this.llShowPersonInfo.AutoSize = true;
+            this.llShowPersonInfo.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.llShowPersonInfo.Location = new System.Drawing.Point(529, 171);
+            this.llShowPersonInfo.Name = "llShowPersonInfo";
+            this.llShowPersonInfo.Size = new System.Drawing.Size(142, 19);
+            this.llShowPersonInfo.TabIndex = 55;
+            this.llShowPersonInfo.TabStop = true;
+            this.llShowPersonInfo.Text = "Show Person Info";
+            this.llShowPersonInfo.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llShowPersonInfo_LinkClicked);
+            // 
+            // txtDate
+            // 
+            this.txtDate.AutoSize = true;
+            this.txtDate.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtDate.ForeColor = System.Drawing.Color.Black;
+            this.txtDate.Location = new System.Drawing.Point(550, 45);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(79, 19);
+            this.txtDate.TabIndex = 66;
+            this.txtDate.Text = "??/??/????";
+            // 
+            // txtStatusDate
+            // 
+            this.txtStatusDate.AutoSize = true;
+            this.txtStatusDate.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtStatusDate.ForeColor = System.Drawing.Color.Black;
+            this.txtStatusDate.Location = new System.Drawing.Point(550, 77);
+            this.txtStatusDate.Name = "txtStatusDate";
+            this.txtStatusDate.Size = new System.Drawing.Size(79, 19);
+            this.txtStatusDate.TabIndex = 65;
+            this.txtStatusDate.Text = "??/??/????";
+            // 
+            // txtCreatedBy
+            // 
+            this.txtCreatedBy.AutoSize = true;
+            this.txtCreatedBy.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtCreatedBy.ForeColor = System.Drawing.Color.Black;
+            this.txtCreatedBy.Location = new System.Drawing.Point(550, 115);
+            this.txtCreatedBy.Name = "txtCreatedBy";
+            this.txtCreatedBy.Size = new System.Drawing.Size(23, 19);
+            this.txtCreatedBy.TabIndex = 64;
+            this.txtCreatedBy.Text = "??";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label10.Location = new System.Drawing.Point(445, 115);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 19);
+            this.label10.TabIndex = 63;
+            this.label10.Text = "Created By :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(487, 45);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 19);
+            this.label9.TabIndex = 62;
+            this.label9.Text = " Date :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(440, 81);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 19);
+            this.label8.TabIndex = 61;
+            this.label8.Text = "Status Date :";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.AutoSize = true;
+            this.txtStatus.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtStatus.ForeColor = System.Drawing.Color.Black;
+            this.txtStatus.Location = new System.Drawing.Point(106, 77);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(23, 19);
+            this.txtStatus.TabIndex = 60;
+            this.txtStatus.Text = "??";
+            // 
+            // txtFees
+            // 
+            this.txtFees.AutoSize = true;
+            this.txtFees.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtFees.ForeColor = System.Drawing.Color.Black;
+            this.txtFees.Location = new System.Drawing.Point(106, 115);
+            this.txtFees.Name = "txtFees";
+            this.txtFees.Size = new System.Drawing.Size(23, 19);
+            this.txtFees.TabIndex = 59;
+            this.txtFees.Text = "??";
+            // 
+            // txtType
+            // 
+            this.txtType.AutoSize = true;
+            this.txtType.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtType.ForeColor = System.Drawing.Color.Black;
+            this.txtType.Location = new System.Drawing.Point(106, 154);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(23, 19);
+            this.txtType.TabIndex = 58;
+            this.txtType.Text = "??";
+            // 
+            // txtApplicant
+            // 
+            this.txtApplicant.AutoSize = true;
+            this.txtApplicant.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtApplicant.ForeColor = System.Drawing.Color.Black;
+            this.txtApplicant.Location = new System.Drawing.Point(106, 189);
+            this.txtApplicant.Name = "txtApplicant";
+            this.txtApplicant.Size = new System.Drawing.Size(23, 19);
+            this.txtApplicant.TabIndex = 57;
+            this.txtApplicant.Text = "??";
+            // 
+            // txtID
+            // 
+            this.txtID.AutoSize = true;
+            this.txtID.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.txtID.ForeColor = System.Drawing.Color.Black;
+            this.txtID.Location = new System.Drawing.Point(106, 45);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(23, 19);
+            this.txtID.TabIndex = 56;
+            this.txtID.Text = "??";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(37, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 19);
+            this.label7.TabIndex = 55;
+            this.label7.Text = "Type :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(2, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 19);
+            this.label6.TabIndex = 54;
+            this.label6.Text = "Applicant :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(27, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 19);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "Status :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(41, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 19);
+            this.label4.TabIndex = 52;
+            this.label4.Text = "Fees :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(57, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 19);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "ID :";
+            // 
+            // UserControl_ApplicationBasic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.grA);
+            this.Name = "UserControl_ApplicationBasic";
+            this.Size = new System.Drawing.Size(790, 248);
+            this.Load += new System.EventHandler(this.UserControl_ApplicationBasic_Load);
+            this.grA.ResumeLayout(false);
+            this.grA.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grA;
+        private System.Windows.Forms.LinkLabel llShowPersonInfo;
+        private System.Windows.Forms.Label txtDate;
+        private System.Windows.Forms.Label txtStatusDate;
+        private System.Windows.Forms.Label txtCreatedBy;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label txtStatus;
+        private System.Windows.Forms.Label txtFees;
+        private System.Windows.Forms.Label txtType;
+        private System.Windows.Forms.Label txtApplicant;
+        private System.Windows.Forms.Label txtID;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ DVLD/Applications/controlls/UserControl_ApplicationBasic.cs
@@ -0,0 +1,86 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Applications.controlls
+{
+    public partial class UserControl_ApplicationBasic : UserControl
+    {
+
+
+        private clsApplication _Application;
+
+        private int _ApplicationID = -1;
+
+        public int ApplicationID
+        {
+            get { return _ApplicationID; }
+        }
+        public void LoadApplicationInfo(int ApplicationID)
+        {
+            _Application = clsApplication.FindBaseApplication(ApplicationID);
+            if (_Application == null)
+            {
+                ResetApplicationInfo();
+                MessageBox.Show("No Application with ApplicationID = " + ApplicationID.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                _FillApplicationInfo();
+        }
+
+        private void _FillApplicationInfo()
+        {
+            _ApplicationID = _Application.ApplicationID;
+            txtID.Text = _Application.ApplicationID.ToString();
+            txtStatus.Text = _Application.StatusText;
+            txtType.Text = _Application.ApplicationTypeInfo.Titel;
+            txtFees.Text = _Application.PaidFees.ToString();
+            txtApplicant.Text = _Application.ApplicantFullName;
+            txtDate.Text = (_Application.ApplicationDate).ToShortDateString();
+            txtStatusDate.Text = (_Application.LastStatusDate).ToShortDateString();
+            txtCreatedBy.Text = _Application.CreatedByUserInfo.UserName;
+        }
+
+        public void ResetApplicationInfo()
+        {
+            _ApplicationID = -1;
+
+            txtID.Text = "??";
+            txtStatus.Text = "??";
+            txtType.Text = "??";
+            txtFees.Text = "??";
+            txtApplicant.Text = "??";
+            txtDate.Text = "??";
+            txtStatusDate.Text = "??";
+            txtCreatedBy.Text = "??";
+
+        }
+
+        
+        public UserControl_ApplicationBasic()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_ApplicationBasic_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void llShowPersonInfo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ShowPersonDetails frm = new ShowPersonDetails(_Application.ApplicantPersonID);
+            frm.ShowDialog();
+
+            //Refresh
+            LoadApplicationInfo(_ApplicationID);
+        }
+    }
+}
