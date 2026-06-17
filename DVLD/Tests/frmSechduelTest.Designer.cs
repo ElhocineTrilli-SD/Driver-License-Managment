2875cff92e16acab1ef5399faa98a908bf0dd54f
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-17 09:36:23 +0100

    add form to show Sechduel test info

---
+++ DVLD/Tests/frmSechduelTest.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace DVLD.Tests
+{
+    partial class frmSechduelTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnclose = new Guna.UI2.WinForms.Guna2Button();
+            this.ctrlSechduelTest1 = new DVLD.Tests.Controlls.ctrlSechduelTest();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.BorderRadius = 10;
+            this.btnclose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnclose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnclose.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnclose.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.ForeColor = System.Drawing.Color.Black;
+            this.btnclose.Location = new System.Drawing.Point(172, 585);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(163, 39);
+            this.btnclose.TabIndex = 78;
+            this.btnclose.Text = "Close";
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // ctrlSechduelTest1
+            // 
+            this.ctrlSechduelTest1.Location = new System.Drawing.Point(32, 2);
+            this.ctrlSechduelTest1.Name = "ctrlSechduelTest1";
+            this.ctrlSechduelTest1.Size = new System.Drawing.Size(467, 567);
+            this.ctrlSechduelTest1.TabIndex = 79;
+            this.ctrlSechduelTest1.TestTypeID = DVLD_BUSINESS.clsTestType.enTestType.VisionTest;
+            // 
+            // frmSechduelTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(535, 636);
+            this.Controls.Add(this.ctrlSechduelTest1);
+            this.Controls.Add(this.btnclose);
+            this.Name = "frmSechduelTest";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sechduel Test";
+            this.Load += new System.EventHandler(this.frmSechduelTest_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btnclose;
+        private Controlls.ctrlSechduelTest ctrlSechduelTest1;
+    }
+}
---
+++ DVLD/Tests/frmSechduelTest.cs
@@ -0,0 +1,42 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Tests
+{
+    public partial class frmSechduelTest : Form
+    {
+        private int _LocalDrivingLicenseApplication = -1;
+        private int _TestAppoitment = -1;
+        private clsTestType.enTestType _testTypeID = clsTestType.enTestType.VisionTest;
+
+
+        public frmSechduelTest(int Local,int Test,clsTestType.enTestType testType)
+        {
+
+            InitializeComponent();
+            _LocalDrivingLicenseApplication = Local;
+            _TestAppoitment = Test;
+            _testTypeID = testType;
+        }
+
+        private void frmSechduelTest_Load(object sender, EventArgs e)
+        {
+            ctrlSechduelTest1.TestTypeID = _testTypeID;
+            ctrlSechduelTest1.LoadInfo(_LocalDrivingLicenseApplication, _TestAppoitment);
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
