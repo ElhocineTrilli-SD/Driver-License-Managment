ad685abdda00cbcfa2df5b2e3492626dc8a3f82b
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-18 15:34:45 +0100

    add   License Business & Data Access

---
+++ DVLD_BUSINESS/clsLicense.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BUSINESS
+{
+    public class clsLicense
+    {
+
+
+        public static int GetActiveLicenseIDByPersonID(int  personID, int licenseClassID)
+        {
+            return clsLicenseData.GetActiveLicenseIDByPersonID(personID, licenseClassID);
+        }
+
+
+    }
+}
