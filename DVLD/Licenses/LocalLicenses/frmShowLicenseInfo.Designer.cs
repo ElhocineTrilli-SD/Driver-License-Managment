b2a773d281b0311bd9de02eefca92453090c69d3
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-19 09:42:22 +0100

    Add new form for showing license info and implement the display logic in the form.

---
+++ DVLD/Licenses/LocalLicenses/frmShowLicenseInfo.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DVLD.Licenses.LocalLicenses
+{
+    partial class frmShowLicenseInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctrlLicenseInfo1 = new DVLD.Licenses.Control.ctrlLicenseInfo();
+            this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.Location = new System.Drawing.Point(556, 429);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(148, 34);
+            this.guna2Button1.TabIndex = 4;
+            this.guna2Button1.Text = "Close";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(256, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 36);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "License Info";
+            // 
+            // ctrlLicenseInfo1
+            // 
+            this.ctrlLicenseInfo1.Location = new System.Drawing.Point(10, 80);
+            this.ctrlLicenseInfo1.Name = "ctrlLicenseInfo1";
+            this.ctrlLicenseInfo1.Size = new System.Drawing.Size(694, 343);
+            this.ctrlLicenseInfo1.TabIndex = 5;
+            // 
+            // frmShowLicenseInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 472);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ctrlLicenseInfo1);
+            this.Controls.Add(this.guna2Button1);
+            this.Name = "frmShowLicenseInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "License Info";
+            this.Load += new System.EventHandler(this.frmShowLicenseInfo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Control.ctrlLicenseInfo ctrlLicenseInfo1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DVLD/Licenses/LocalLicenses/frmShowLicenseInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Licenses.LocalLicenses
+{
+    public partial class frmShowLicenseInfo : Form
+    {
+        public int _LicenseID = -1;
+        public frmShowLicenseInfo(int LicenesID)
+        {
+            InitializeComponent();
+            _LicenseID = LicenesID;
+        }
+
+        private void frmShowLicenseInfo_Load(object sender, EventArgs e)
+        {
+            ctrlLicenseInfo1.LoadInfo(_LicenseID);
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
