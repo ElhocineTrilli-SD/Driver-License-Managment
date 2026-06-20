2550229affef1e861aadb277d2f30c5791eba4da
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-20 05:43:42 +0100

    Add user control  to find license ID.

---
+++ DVLD/Licenses/Control/ctrlLicenseInfoWithFilter.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DVLD.Licenses.Control
+{
+    partial class ctrlLicenseInfoWithFilter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ctrlLicenseInfo1 = new DVLD.Licenses.Control.ctrlLicenseInfo();
+            this.Filter = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFilter = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnSearch = new Guna.UI2.WinForms.Guna2Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.Filter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ctrlLicenseInfo1
+            // 
+            this.ctrlLicenseInfo1.Location = new System.Drawing.Point(0, 98);
+            this.ctrlLicenseInfo1.Name = "ctrlLicenseInfo1";
+            this.ctrlLicenseInfo1.Size = new System.Drawing.Size(706, 341);
+            this.ctrlLicenseInfo1.TabIndex = 0;
+            // 
+            // Filter
+            // 
+            this.Filter.Controls.Add(this.btnSearch);
+            this.Filter.Controls.Add(this.txtFilter);
+            this.Filter.Controls.Add(this.label5);
+            this.Filter.Location = new System.Drawing.Point(3, 3);
+            this.Filter.Name = "Filter";
+            this.Filter.Size = new System.Drawing.Size(391, 89);
+            this.Filter.TabIndex = 1;
+            this.Filter.TabStop = false;
+            this.Filter.Text = "Filter";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(6, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 19);
+            this.label5.TabIndex = 82;
+            this.label5.Text = "LicenseID :";
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtFilter.DefaultText = "";
+            this.txtFilter.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtFilter.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtFilter.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFilter.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFilter.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFilter.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtFilter.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFilter.Location = new System.Drawing.Point(101, 38);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.PlaceholderText = "";
+            this.txtFilter.SelectedText = "";
+            this.txtFilter.Size = new System.Drawing.Size(193, 19);
+            this.txtFilter.TabIndex = 83;
+            this.txtFilter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilter_KeyPress);
+            this.txtFilter.Validating += new System.ComponentModel.CancelEventHandler(this.txtFilter_Validating);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BorderRadius = 10;
+            this.btnSearch.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSearch.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSearch.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSearch.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSearch.FillColor = System.Drawing.Color.White;
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Image = global::DVLD.Properties.Resources.magnifying_glass1;
+            this.btnSearch.ImageOffset = new System.Drawing.Point(1, 0);
+            this.btnSearch.ImageSize = new System.Drawing.Size(28, 28);
+            this.btnSearch.Location = new System.Drawing.Point(330, 19);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(45, 38);
+            this.btnSearch.TabIndex = 84;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ctrlLicenseInfoWithFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Filter);
+            this.Controls.Add(this.ctrlLicenseInfo1);
+            this.Name = "ctrlLicenseInfoWithFilter";
+            this.Size = new System.Drawing.Size(699, 438);
+            this.Filter.ResumeLayout(false);
+            this.Filter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlLicenseInfo ctrlLicenseInfo1;
+        private System.Windows.Forms.GroupBox Filter;
+        private Guna.UI2.WinForms.Guna2TextBox txtFilter;
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2Button btnSearch;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ DVLD/Licenses/Control/ctrlLicenseInfoWithFilter.cs
@@ -0,0 +1,107 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Licenses.Control
+{
+    public partial class ctrlLicenseInfoWithFilter : UserControl
+    {
+        // Define a custom event handler delegate with parameters
+        public event Action<int> OnLicenseSelected;
+        // Create a protected method to raise the event with a parameter
+        protected virtual void LicenseSelected(int LicenseID)
+        {
+            Action<int> handler = OnLicenseSelected;
+            if (handler != null)
+            {
+                handler(LicenseID); // Raise the event with the parameter
+            }
+        }
+
+        private int _LicenseID;
+        private bool _FilterEnabled = true;
+
+        public bool FilterEnabled
+        {
+            get { return _FilterEnabled; }
+            set {
+                _FilterEnabled = value;
+                Filter.Enabled = _FilterEnabled;
+            }
+        }
+
+        public int LicenseID
+        {
+            get { return ctrlLicenseInfo1.LicenseID; }
+        }
+
+        public clsLicense SelectedLicenseInfo
+        {
+            get { return ctrlLicenseInfo1.SelectedLicenseInfo;  }
+        }
+
+        public void LoadLicenseInfo(int licenseID)
+        {
+            txtFilter.Text = licenseID.ToString();
+            ctrlLicenseInfo1.LoadInfo(licenseID);
+            _LicenseID = ctrlLicenseInfo1.LicenseID;
+            if(OnLicenseSelected != null && FilterEnabled)
+            {
+                LicenseSelected(licenseID);
+            }
+        }
+
+
+        public ctrlLicenseInfoWithFilter()
+        {
+            InitializeComponent();
+        }
+        public void FilterFocus()
+        {
+            txtFilter.Focus();
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren())
+            {
+                //Here we dont continue becuase the form is not valid
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+            _LicenseID = int.Parse(txtFilter.Text);
+            ctrlLicenseInfo1.LoadInfo(_LicenseID);
+        }
+
+        private void txtFilter_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFilter.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtFilter, "This Field is required!");
+            }
+            else
+            {
+                errorProvider1.SetError(txtFilter, null);
+            }
+        }
+
+        private void txtFilter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);  
+
+            if(e.KeyChar == (char)13)
+            {
+                btnSearch.PerformClick();
+            }
+
+        }
+    }
+}
