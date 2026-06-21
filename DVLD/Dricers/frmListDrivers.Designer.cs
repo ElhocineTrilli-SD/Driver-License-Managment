ae65a56a8473326bcb48a6beffd8b613869fa2d2
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-21 07:50:34 +0100

    add new form for  drivers List

---
+++ DVLD/Dricers/frmListDrivers.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace DVLD.Dricers
+{
+    partial class frmListDrivers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnclose = new Guna.UI2.WinForms.Guna2Button();
+            this.lblRecordsCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvDrivers = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbfilter = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFilterValue = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showDriverInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showPersonLicenseHistoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDrivers)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.BorderRadius = 10;
+            this.btnclose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnclose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnclose.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnclose.Location = new System.Drawing.Point(653, 462);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(155, 53);
+            this.btnclose.TabIndex = 13;
+            this.btnclose.Text = "Close";
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // lblRecordsCount
+            // 
+            this.lblRecordsCount.AutoSize = true;
+            this.lblRecordsCount.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordsCount.Location = new System.Drawing.Point(115, 459);
+            this.lblRecordsCount.Name = "lblRecordsCount";
+            this.lblRecordsCount.Size = new System.Drawing.Size(17, 19);
+            this.lblRecordsCount.TabIndex = 12;
+            this.lblRecordsCount.Text = "?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 459);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 19);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "#Records: ";
+            // 
+            // dgvDrivers
+            // 
+            this.dgvDrivers.AllowUserToAddRows = false;
+            this.dgvDrivers.AllowUserToDeleteRows = false;
+            this.dgvDrivers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDrivers.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgvDrivers.Location = new System.Drawing.Point(31, 165);
+            this.dgvDrivers.Name = "dgvDrivers";
+            this.dgvDrivers.ReadOnly = true;
+            this.dgvDrivers.Size = new System.Drawing.Size(759, 291);
+            this.dgvDrivers.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(290, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 36);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Manage Drivers";
+            // 
+            // cbfilter
+            // 
+            this.cbfilter.FormattingEnabled = true;
+            this.cbfilter.Location = new System.Drawing.Point(119, 127);
+            this.cbfilter.Name = "cbfilter";
+            this.cbfilter.Size = new System.Drawing.Size(161, 21);
+            this.cbfilter.TabIndex = 16;
+            this.cbfilter.SelectedIndexChanged += new System.EventHandler(this.cbfilter_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 22);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Filter By: ";
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.Location = new System.Drawing.Point(296, 127);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.Size = new System.Drawing.Size(137, 20);
+            this.txtFilterValue.TabIndex = 18;
+            this.txtFilterValue.Visible = false;
+            this.txtFilterValue.TextChanged += new System.EventHandler(this.txtFilterValue_TextChanged);
+            this.txtFilterValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilterValue_KeyPress);
+            this.txtFilterValue.Validating += new System.ComponentModel.CancelEventHandler(this.txtFilterValue_Validating);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showDriverInfoToolStripMenuItem,
+            this.showPersonLicenseHistoryToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(341, 94);
+            // 
+            // showDriverInfoToolStripMenuItem
+            // 
+            this.showDriverInfoToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15.75F);
+            this.showDriverInfoToolStripMenuItem.Name = "showDriverInfoToolStripMenuItem";
+            this.showDriverInfoToolStripMenuItem.Size = new System.Drawing.Size(340, 34);
+            this.showDriverInfoToolStripMenuItem.Text = "Show Person Info";
+            this.showDriverInfoToolStripMenuItem.Click += new System.EventHandler(this.showDriverInfoToolStripMenuItem_Click);
+            // 
+            // showPersonLicenseHistoryToolStripMenuItem
+            // 
+            this.showPersonLicenseHistoryToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15.75F);
+            this.showPersonLicenseHistoryToolStripMenuItem.Name = "showPersonLicenseHistoryToolStripMenuItem";
+            this.showPersonLicenseHistoryToolStripMenuItem.Size = new System.Drawing.Size(340, 34);
+            this.showPersonLicenseHistoryToolStripMenuItem.Text = "Show Person License history";
+            // 
+            // frmListDrivers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(820, 527);
+            this.Controls.Add(this.txtFilterValue);
+            this.Controls.Add(this.cbfilter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.lblRecordsCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDrivers);
+            this.Name = "frmListDrivers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "List Drivers";
+            this.Load += new System.EventHandler(this.frmListDrivers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDrivers)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btnclose;
+        private System.Windows.Forms.Label lblRecordsCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvDrivers;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbfilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showDriverInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showPersonLicenseHistoryToolStripMenuItem;
+    }
+}
---
+++ DVLD/Dricers/frmListDrivers.cs
@@ -0,0 +1,174 @@
+﻿using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Dricers
+{
+
+    public partial class frmListDrivers : Form
+    {
+
+        private DataTable _DtDrivers;
+
+
+        public frmListDrivers()
+        {
+            InitializeComponent();
+        }
+        public void _RefrechDriversList()
+        {
+            _DtDrivers = clsDrivers.GetAllDrivers();
+            dgvDrivers.DataSource = _DtDrivers;
+            _RefrechRecordCount();
+        }
+
+        private void _RefrechRecordCount()
+        {
+            int Record = dgvDrivers.RowCount;
+            lblRecordsCount.Text = Record.ToString();
+
+        }
+
+        private void frmListDrivers_Load(object sender, EventArgs e)
+        {
+            _RefrechDriversList();
+            _FillcbfilteringwithItems();
+            if (dgvDrivers.RowCount > 0)
+            {
+                dgvDrivers.Columns[0].HeaderText = "Driver ID";
+                dgvDrivers.Columns[0].Width = 90;
+
+                dgvDrivers.Columns[1].HeaderText = "Person ID";
+                dgvDrivers.Columns[1].Width = 90;
+
+                dgvDrivers.Columns[2].HeaderText = "NationalNo";
+                dgvDrivers.Columns[2].Width = 90;
+
+                dgvDrivers.Columns[3].HeaderText = "Full Name";
+                dgvDrivers.Columns[3].Width = 250;
+
+                dgvDrivers.Columns[4].HeaderText = "Date";
+                dgvDrivers.Columns[4].Width = 100;
+
+                dgvDrivers.Columns[5].HeaderText = "Is Active";
+                dgvDrivers.Columns[5].Width = 90;
+
+            }
+        }
+
+        private void _FillcbfilteringwithItems()
+        {
+            cbfilter.Items.Add("None");
+            cbfilter.Items.Add("Driver ID");
+            cbfilter.Items.Add("Person ID");
+            cbfilter.Items.Add("NationalNo");
+            cbfilter.Items.Add("Full Name");
+
+            cbfilter.SelectedIndex = 0;
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtFilterValue_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void txtFilterValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (cbfilter.Text == "Driver ID" || cbfilter.Text == "Person ID")
+            {
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+
+            }
+        }
+
+        private void cbfilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          txtFilterValue.Visible = (cbfilter.SelectedIndex != 0);
+
+            if(cbfilter.Text == "None")
+            {
+                txtFilterValue.Enabled = false;
+            }
+            else
+            {
+                txtFilterValue.Enabled = true;
+                txtFilterValue.Focus();
+                txtFilterValue.Text = "";
+            }
+
+        }
+
+        private void txtFilterValue_TextChanged(object sender, EventArgs e)
+        {
+            string FilterColumn = "";
+            //Map Selected Filter to real Column name 
+            switch (cbfilter.Text)
+            {
+                case "Driver ID":
+                    FilterColumn = "DriverID";
+                    break;
+                case "Person ID":
+                    FilterColumn = "PersonID";
+                    break;
+
+                case "NationalNo":
+                    FilterColumn = "NationalNo";
+                    break;
+
+
+                case "Full Name":
+                    FilterColumn = "FullName";
+                    break;
+
+                //case "Date":
+                //    FilterColumn = "Date";
+                //    break;
+
+                default:
+                    FilterColumn = "None";
+                    break;
+
+            }
+
+            if (txtFilterValue.Text == "" || FilterColumn == "None")
+            {
+                _DtDrivers.DefaultView.RowFilter = "";
+                lblRecordsCount.Text = _DtDrivers.Rows.Count.ToString();
+                return;
+            }
+
+            if (FilterColumn != "FullName" || FilterColumn != "NationalNo")
+            {
+                _DtDrivers.DefaultView.RowFilter = string.Format("[{0}] = {1}", FilterColumn, txtFilterValue.Text.Trim());
+
+            }
+            else
+                _DtDrivers.DefaultView.RowFilter = string.Format("[{0}] LIKE '{1}%'", FilterColumn, txtFilterValue.Text.Trim());
+
+            lblRecordsCount.Text = _DtDrivers.Rows.Count.ToString();
+
+        }
+
+        private void showDriverInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            ShowPersonDetails showPerson = new ShowPersonDetails((int)dgvDrivers.CurrentRow.Cells[1].Value);
+            showPerson.ShowDialog();
+        }
+    }
+    
+}
