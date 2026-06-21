3e75edd9d12c5e3808ef2dfeb4d89e1abeeb82c9
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-21 09:26:15 +0100

    add person license history form

---
+++ DVLD/Licenses/frmShowPersonLicenseHistory.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DVLD.Licenses
+{
+    partial class frmShowPersonLicenseHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlDriverLicenses1 = new DVLD.Licenses.Control.ctrlDriverLicenses();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.personInfoWithFilter11 = new DVLD.People.Controlls.PersonInfoWithFilter1();
+            this.SuspendLayout();
+            // 
+            // ctrlDriverLicenses1
+            // 
+            this.ctrlDriverLicenses1.Location = new System.Drawing.Point(18, 380);
+            this.ctrlDriverLicenses1.Name = "ctrlDriverLicenses1";
+            this.ctrlDriverLicenses1.Size = new System.Drawing.Size(766, 261);
+            this.ctrlDriverLicenses1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "License ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(22, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 36);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "History";
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.Location = new System.Drawing.Point(666, 662);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(148, 34);
+            this.guna2Button1.TabIndex = 9;
+            this.guna2Button1.Text = "Close";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // personInfoWithFilter11
+            // 
+            this.personInfoWithFilter11.FilterEnabled = true;
+            this.personInfoWithFilter11.Location = new System.Drawing.Point(213, 12);
+            this.personInfoWithFilter11.Name = "personInfoWithFilter11";
+            this.personInfoWithFilter11.ShowAddPerson = true;
+            this.personInfoWithFilter11.Size = new System.Drawing.Size(637, 362);
+            this.personInfoWithFilter11.TabIndex = 10;
+            this.personInfoWithFilter11.OnPersonSelected += new System.Action<int>(this.personInfoWithFilter11_OnPersonSelected);
+            // 
+            // frmShowPersonLicenseHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 703);
+            this.Controls.Add(this.personInfoWithFilter11);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ctrlDriverLicenses1);
+            this.Name = "frmShowPersonLicenseHistory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Show License History";
+            this.Load += new System.EventHandler(this.frmShowPersonLicenseHistory_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Control.ctrlDriverLicenses ctrlDriverLicenses1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private People.Controlls.PersonInfoWithFilter1 personInfoWithFilter11;
+    }
+}
---
+++ DVLD/Licenses/frmShowPersonLicenseHistory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Licenses
+{
+    public partial class frmShowPersonLicenseHistory : Form
+    {
+        private int _PersonID = -1;
+        public frmShowPersonLicenseHistory()
+        {
+            InitializeComponent();
+        }
+        public frmShowPersonLicenseHistory(int PersonID)
+        {
+            InitializeComponent();
+            _PersonID = PersonID;
+        }
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmShowPersonLicenseHistory_Load(object sender, EventArgs e)
+        {
+            if(_PersonID != -1)
+            {
+                personInfoWithFilter11.LoadPersonInfo(_PersonID);
+                personInfoWithFilter11.FilterEnabled = false;
+                ctrlDriverLicenses1.LoadInfoByPersonId(_PersonID);
+            }
+            else
+            {
+                personInfoWithFilter11.Enabled = true;
+                personInfoWithFilter11.FilterFocus();
+            }
+                
+
+
+        }
+
+   
+        private void personInfoWithFilter11_OnPersonSelected(int obj)
+        {
+            _PersonID = obj;
+            if (_PersonID == -1)
+            {
+                ctrlDriverLicenses1.Clear();
+
+            }
+            else
+            {
+                ctrlDriverLicenses1.LoadInfoByPersonId(_PersonID);
+            }
+        }
+    }
+}
