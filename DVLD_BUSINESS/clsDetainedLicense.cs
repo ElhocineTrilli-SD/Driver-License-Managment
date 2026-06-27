588ea35fe007bec919026b86d9feef8f0d1282ff
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-27 14:15:23 +0100

    add business part to detained licenses

---
+++ DVLD_BUSINESS/clsDetainedLicense.cs
@@ -0,0 +1,87 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BUSINESS
+{
+    public class clsDetainedLicense
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+
+        public int DetainID { set; get; }
+        public int LicenseID { set; get; }
+        public DateTime DetainDate { set; get; }
+
+        public float FineFees { set; get; }
+        public int CreatedByUserID { set; get; }
+        public clsUser CreatedByUserInfo { set; get; }
+        public bool IsReleased { set; get; }
+        public DateTime ReleaseDate { set; get; }
+        public int ReleasedByUserID { set; get; }
+        public clsUser ReleasedByUserInfo { set; get; }
+        public int ReleaseApplicationID { set; get; }
+
+        public clsDetainedLicense()
+
+        {
+            this.DetainID = -1;
+            this.LicenseID = -1;
+            this.DetainDate = DateTime.Now;
+            this.FineFees = 0;
+            this.CreatedByUserID = -1;
+            this.IsReleased = false;
+            this.ReleaseDate = DateTime.MaxValue;
+            this.ReleasedByUserID = 0;
+            this.ReleaseApplicationID = -1;
+
+
+
+            Mode = enMode.AddNew;
+
+        }
+
+        public clsDetainedLicense(int DetainID,
+            int LicenseID, DateTime DetainDate,
+            float FineFees, int CreatedByUserID,
+            bool IsReleased, DateTime ReleaseDate,
+            int ReleasedByUserID, int ReleaseApplicationID)
+
+        {
+            this.DetainID = DetainID;
+            this.LicenseID = LicenseID;
+            this.DetainDate = DetainDate;
+            this.FineFees = FineFees;
+            this.CreatedByUserID = CreatedByUserID;
+            this.CreatedByUserInfo = clsUser.FindByUserID(this.CreatedByUserID);
+            this.IsReleased = IsReleased;
+            this.ReleaseDate = ReleaseDate;
+            this.ReleasedByUserID = ReleasedByUserID;
+            this.ReleaseApplicationID = ReleaseApplicationID;
+            this.ReleasedByUserInfo = clsUser.FindByPersonID(this.ReleasedByUserID);
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewDetainedLicense()
+        {
+            //call DataAccess Layer 
+
+            this.DetainID = clsDetainLicenseData.AddNewDetainedLicense(
+                this.LicenseID, this.DetainDate, this.FineFees, this.CreatedByUserID);
+
+            return (this.DetainID != -1);
+        }
+
+        private bool _UpdateDetainedLicense()
+        {
+            //call DataAccess Layer 
+
+            return clsDetainLicenseData.UpdateDetainedLicense(
+                this.DetainID, this.LicenseID, this.DetainDate, this.FineFees, this.CreatedByUserID);
+        }
+    }
+}
