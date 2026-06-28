8e0243836af5cf4f9e57e318ba06be83de6d5d35
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-28 08:27:12 +0100

    add form to manage DetainLisense

---
+++ DVLD/Licenses/DetainedLisenses/frmDetainLicenseApplication.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace DVLD.Licenses.DetainedLisenses
+{
+    partial class frmDetainLicenseApplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ctrlLicenseInfoWithFilter1 = new DVLD.Licenses.Control.ctrlLicenseInfoWithFilter();
+            this.gbDetainInfo = new System.Windows.Forms.GroupBox();
+            this.txtFineFees = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lblCreatedBy = new System.Windows.Forms.Label();
+            this.lblLicenseID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDtainDate = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.lblDetainID = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.btnDetain = new Guna.UI2.WinForms.Guna2Button();
+            this.btnclose = new Guna.UI2.WinForms.Guna2Button();
+            this.llblShowHistory = new System.Windows.Forms.LinkLabel();
+            this.llblShowLicenseInfo = new System.Windows.Forms.LinkLabel();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.lblTitel = new System.Windows.Forms.Label();
+            this.gbDetainInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ctrlLicenseInfoWithFilter1
+            // 
+            this.ctrlLicenseInfoWithFilter1.FilterEnabled = true;
+            this.ctrlLicenseInfoWithFilter1.Location = new System.Drawing.Point(20, 0);
+            this.ctrlLicenseInfoWithFilter1.Name = "ctrlLicenseInfoWithFilter1";
+            this.ctrlLicenseInfoWithFilter1.Size = new System.Drawing.Size(699, 438);
+            this.ctrlLicenseInfoWithFilter1.TabIndex = 0;
+            this.ctrlLicenseInfoWithFilter1.OnLicenseSelected += new System.Action<int>(this.SelectedLicenseID);
+            // 
+            // gbDetainInfo
+            // 
+            this.gbDetainInfo.Controls.Add(this.txtFineFees);
+            this.gbDetainInfo.Controls.Add(this.lblCreatedBy);
+            this.gbDetainInfo.Controls.Add(this.lblLicenseID);
+            this.gbDetainInfo.Controls.Add(this.label3);
+            this.gbDetainInfo.Controls.Add(this.label4);
+            this.gbDetainInfo.Controls.Add(this.lblDtainDate);
+            this.gbDetainInfo.Controls.Add(this.label15);
+            this.gbDetainInfo.Controls.Add(this.lblDetainID);
+            this.gbDetainInfo.Controls.Add(this.label18);
+            this.gbDetainInfo.Controls.Add(this.label19);
+            this.gbDetainInfo.Location = new System.Drawing.Point(20, 444);
+            this.gbDetainInfo.Name = "gbDetainInfo";
+            this.gbDetainInfo.Size = new System.Drawing.Size(690, 137);
+            this.gbDetainInfo.TabIndex = 1;
+            this.gbDetainInfo.TabStop = false;
+            this.gbDetainInfo.Text = "Detain Info";
+            // 
+            // txtFineFees
+            // 
+            this.txtFineFees.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtFineFees.DefaultText = "";
+            this.txtFineFees.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtFineFees.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtFineFees.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFineFees.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFineFees.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFineFees.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtFineFees.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFineFees.Location = new System.Drawing.Point(121, 102);
+            this.txtFineFees.Name = "txtFineFees";
+            this.txtFineFees.PlaceholderText = "";
+            this.txtFineFees.SelectedText = "";
+            this.txtFineFees.Size = new System.Drawing.Size(117, 17);
+            this.txtFineFees.TabIndex = 111;
+            this.txtFineFees.Validating += new System.ComponentModel.CancelEventHandler(this.txtFineFees_Validating);
+            // 
+            // lblCreatedBy
+            // 
+            this.lblCreatedBy.AutoSize = true;
+            this.lblCreatedBy.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCreatedBy.ForeColor = System.Drawing.Color.Black;
+            this.lblCreatedBy.Location = new System.Drawing.Point(503, 66);
+            this.lblCreatedBy.Name = "lblCreatedBy";
+            this.lblCreatedBy.Size = new System.Drawing.Size(21, 19);
+            this.lblCreatedBy.TabIndex = 110;
+            this.lblCreatedBy.Text = "??";
+            // 
+            // lblLicenseID
+            // 
+            this.lblLicenseID.AutoSize = true;
+            this.lblLicenseID.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblLicenseID.ForeColor = System.Drawing.Color.Black;
+            this.lblLicenseID.Location = new System.Drawing.Point(503, 31);
+            this.lblLicenseID.Name = "lblLicenseID";
+            this.lblLicenseID.Size = new System.Drawing.Size(23, 19);
+            this.lblLicenseID.TabIndex = 109;
+            this.lblLicenseID.Text = "??";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(402, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 19);
+            this.label3.TabIndex = 108;
+            this.label3.Text = "License ID :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(390, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 19);
+            this.label4.TabIndex = 107;
+            this.label4.Text = "Ctreated By :";
+            // 
+            // lblDtainDate
+            // 
+            this.lblDtainDate.AutoSize = true;
+            this.lblDtainDate.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDtainDate.ForeColor = System.Drawing.Color.Black;
+            this.lblDtainDate.Location = new System.Drawing.Point(117, 66);
+            this.lblDtainDate.Name = "lblDtainDate";
+            this.lblDtainDate.Size = new System.Drawing.Size(21, 19);
+            this.lblDtainDate.TabIndex = 106;
+            this.lblDtainDate.Text = "??";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label15.Location = new System.Drawing.Point(23, 101);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(85, 19);
+            this.label15.TabIndex = 105;
+            this.label15.Text = "Fine Fees :";
+            // 
+            // lblDetainID
+            // 
+            this.lblDetainID.AutoSize = true;
+            this.lblDetainID.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblDetainID.ForeColor = System.Drawing.Color.Black;
+            this.lblDetainID.Location = new System.Drawing.Point(117, 31);
+            this.lblDetainID.Name = "lblDetainID";
+            this.lblDetainID.Size = new System.Drawing.Size(23, 19);
+            this.lblDetainID.TabIndex = 104;
+            this.lblDetainID.Text = "??";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label18.Location = new System.Drawing.Point(23, 31);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(88, 19);
+            this.label18.TabIndex = 102;
+            this.label18.Text = "Detain ID :";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label19.Location = new System.Drawing.Point(4, 66);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(107, 19);
+            this.label19.TabIndex = 101;
+            this.label19.Text = "Detain Date :";
+            // 
+            // btnDetain
+            // 
+            this.btnDetain.BorderRadius = 10;
+            this.btnDetain.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnDetain.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnDetain.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnDetain.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnDetain.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetain.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDetain.Location = new System.Drawing.Point(618, 587);
+            this.btnDetain.Name = "btnDetain";
+            this.btnDetain.Size = new System.Drawing.Size(155, 34);
+            this.btnDetain.TabIndex = 101;
+            this.btnDetain.Text = "Detain";
+            this.btnDetain.Click += new System.EventHandler(this.btnDetain_Click);
+            // 
+            // btnclose
+            // 
+            this.btnclose.BorderRadius = 10;
+            this.btnclose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnclose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnclose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnclose.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnclose.Location = new System.Drawing.Point(457, 587);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(155, 34);
+            this.btnclose.TabIndex = 100;
+            this.btnclose.Text = "Close";
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // llblShowHistory
+            // 
+            this.llblShowHistory.AutoSize = true;
+            this.llblShowHistory.Enabled = false;
+            this.llblShowHistory.Location = new System.Drawing.Point(21, 583);
+            this.llblShowHistory.Name = "llblShowHistory";
+            this.llblShowHistory.Size = new System.Drawing.Size(109, 13);
+            this.llblShowHistory.TabIndex = 99;
+            this.llblShowHistory.TabStop = true;
+            this.llblShowHistory.Text = "Show License History";
+            this.llblShowHistory.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llblShowHistory_LinkClicked);
+            // 
+            // llblShowLicenseInfo
+            // 
+            this.llblShowLicenseInfo.AutoSize = true;
+            this.llblShowLicenseInfo.Enabled = false;
+            this.llblShowLicenseInfo.Location = new System.Drawing.Point(143, 583);
+            this.llblShowLicenseInfo.Name = "llblShowLicenseInfo";
+            this.llblShowLicenseInfo.Size = new System.Drawing.Size(95, 13);
+            this.llblShowLicenseInfo.TabIndex = 98;
+            this.llblShowLicenseInfo.TabStop = true;
+            this.llblShowLicenseInfo.Text = "Show License Info";
+            this.llblShowLicenseInfo.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llblShowLicenseInfo_LinkClicked);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // lblTitel
+            // 
+            this.lblTitel.AutoSize = true;
+            this.lblTitel.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lblTitel.ForeColor = System.Drawing.Color.Red;
+            this.lblTitel.Location = new System.Drawing.Point(451, 24);
+            this.lblTitel.Name = "lblTitel";
+            this.lblTitel.Size = new System.Drawing.Size(213, 36);
+            this.lblTitel.TabIndex = 102;
+            this.lblTitel.Text = "Detain License";
+            // 
+            // frmDetainLicenseApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 638);
+            this.Controls.Add(this.lblTitel);
+            this.Controls.Add(this.btnDetain);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.llblShowHistory);
+            this.Controls.Add(this.llblShowLicenseInfo);
+            this.Controls.Add(this.gbDetainInfo);
+            this.Controls.Add(this.ctrlLicenseInfoWithFilter1);
+            this.Name = "frmDetainLicenseApplication";
+            this.Text = "Detain License Application";
+            this.Load += new System.EventHandler(this.frmDetainLicenseApplication_Load);
+            this.gbDetainInfo.ResumeLayout(false);
+            this.gbDetainInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Control.ctrlLicenseInfoWithFilter ctrlLicenseInfoWithFilter1;
+        private System.Windows.Forms.GroupBox gbDetainInfo;
+        private System.Windows.Forms.Label lblDtainDate;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label lblDetainID;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private Guna.UI2.WinForms.Guna2TextBox txtFineFees;
+        private System.Windows.Forms.Label lblCreatedBy;
+        private System.Windows.Forms.Label lblLicenseID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2Button btnDetain;
+        private Guna.UI2.WinForms.Guna2Button btnclose;
+        private System.Windows.Forms.LinkLabel llblShowHistory;
+        private System.Windows.Forms.LinkLabel llblShowLicenseInfo;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label lblTitel;
+    }
+}
---
+++ DVLD/Licenses/DetainedLisenses/frmDetainLicenseApplication.cs
@@ -0,0 +1,132 @@
+﻿using DVLD.Globle_Classes;
+using DVLD.Licenses.LocalLicenses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Licenses.DetainedLisenses
+{
+    public partial class frmDetainLicenseApplication : Form
+    {
+        private int _DetainID = -1;
+        private int _SelectedLicenseID = -1;
+
+        public frmDetainLicenseApplication()
+        {
+            InitializeComponent();
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDetain_Click(object sender, EventArgs e)
+        {
+            //are you sure.
+          if(  MessageBox.Show("Are you sure you want to detain this license!",
+                "Confirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.No     )
+            {
+                return;
+            }
+
+            _DetainID = ctrlLicenseInfoWithFilter1.SelectedLicenseInfo.
+                Detain( Convert.ToSingle(txtFineFees.Text), GlobalClass.CurrentUser.UserID);
+            if (_DetainID == -1)
+            {
+                MessageBox.Show("Faild to Detain License", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            lblDetainID.Text = _DetainID.ToString();
+            MessageBox.Show("License Detained Successfully with ID=" + _DetainID.ToString(), "License Issued", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            btnDetain.Enabled = false;
+            ctrlLicenseInfoWithFilter1.FilterEnabled = false;
+            txtFineFees.Enabled = false;
+            llblShowLicenseInfo.Enabled = true;
+        }
+
+        private void frmDetainLicenseApplication_Load(object sender, EventArgs e)
+        {
+            ctrlLicenseInfoWithFilter1.Focus();
+            lblCreatedBy.Text = GlobalClass.CurrentUser.UserName;
+
+            lblDtainDate.Text = DateTime.Now.ToString("dd MM yyyy");
+
+        }
+
+        private void SelectedLicenseID(int obj)
+        {
+            _SelectedLicenseID = obj;
+            lblLicenseID.Text = _SelectedLicenseID.ToString();
+            llblShowHistory.Enabled = (_SelectedLicenseID != -1);
+
+            if(_SelectedLicenseID ==  -1)
+            {
+                return;
+            }
+
+
+            // malke sure thet id is not already detained;
+            if(ctrlLicenseInfoWithFilter1.SelectedLicenseInfo.IsDetained)
+            {
+                MessageBox.Show("Selected License is Already Detained.choose another one.", "error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            txtFineFees.Focus();
+            btnDetain.Enabled = true;
+        }
+
+        private void llblShowHistory_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmShowPersonLicenseHistory frm =
+                new frmShowPersonLicenseHistory(ctrlLicenseInfoWithFilter1.SelectedLicenseInfo.DriversInfo.PersonID);
+            frm.ShowDialog();
+        }
+
+        private void llblShowLicenseInfo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmShowLicenseInfo frm =
+                new frmShowLicenseInfo(_SelectedLicenseID);
+            frm.ShowDialog();
+        }
+
+        private void txtFineFees_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFineFees.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtFineFees, "Fees cannot be empty!");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(txtFineFees, null);
+
+            }
+            ;
+
+
+            if (!clsValidation.IsNumber(txtFineFees.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtFineFees, "Invalid Number.");
+            }
+            else
+            {
+                errorProvider1.SetError(txtFineFees, null);
+            }
+            ;
+        }
+    }
+}
