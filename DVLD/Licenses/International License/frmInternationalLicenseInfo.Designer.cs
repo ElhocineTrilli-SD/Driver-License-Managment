efd5222c7f9345efa73c244bd5f542cc68dfbbe0
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-29 18:03:23 +0100

    add from to show inter-License info

---
+++ DVLD/Licenses/International License/frmInternationalLicenseInfo.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DVLD.Licenses.International_License
+{
+    partial class frmInternationalLicenseInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlInternationalLicenseInfo1 = new DVLD.Licenses.Control.ctrlInternationalLicenseInfo();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ctrlInternationalLicenseInfo1
+            // 
+            this.ctrlInternationalLicenseInfo1.Location = new System.Drawing.Point(12, 105);
+            this.ctrlInternationalLicenseInfo1.Name = "ctrlInternationalLicenseInfo1";
+            this.ctrlInternationalLicenseInfo1.Size = new System.Drawing.Size(690, 224);
+            this.ctrlInternationalLicenseInfo1.TabIndex = 0;
+            this.ctrlInternationalLicenseInfo1.Load += new System.EventHandler(this.ctrlInternationalLicenseInfo1_Load);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.Location = new System.Drawing.Point(554, 335);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(148, 34);
+            this.guna2Button1.TabIndex = 5;
+            this.guna2Button1.Text = "Close";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(173, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "International License Info";
+            // 
+            // frmInternationalLicenseInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 381);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.ctrlInternationalLicenseInfo1);
+            this.Name = "frmInternationalLicenseInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "International License Info";
+            this.Load += new System.EventHandler(this.frmInternationalLicenseInfo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Control.ctrlInternationalLicenseInfo ctrlInternationalLicenseInfo1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DVLD/Licenses/International License/frmInternationalLicenseInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Licenses.International_License
+{
+    public partial class frmInternationalLicenseInfo : Form
+    {
+        private int _InternationalLicenseID = -1;
+        public frmInternationalLicenseInfo(int ID)
+        {
+            InitializeComponent();
+            _InternationalLicenseID = ID;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ctrlInternationalLicenseInfo1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmInternationalLicenseInfo_Load(object sender, EventArgs e)
+        {
+            ctrlInternationalLicenseInfo1.LoadInfo(_InternationalLicenseID);
+        }
+    }
+}
