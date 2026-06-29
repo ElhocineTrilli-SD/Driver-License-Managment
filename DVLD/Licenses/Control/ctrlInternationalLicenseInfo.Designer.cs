10e744b65d3dbd305063563b76bcd75e3964648d
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-06-29 18:02:24 +0100

    add controll to show international License Info

---
+++ DVLD/Licenses/Control/ctrlInternationalLicenseInfo.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace DVLD.Licenses.Control
+{
+    partial class ctrlInternationalLicenseInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDriver_ID = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblExpirationDate = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblDateOfBirth = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.lblIsActive = new System.Windows.Forms.Label();
+            this.lblAppID = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblPersonName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInter_LicenseID = new System.Windows.Forms.Label();
+            this.lblNationalNo = new System.Windows.Forms.Label();
+            this.lblIssueDate = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblLicenseID = new System.Windows.Forms.Label();
+            this.pbPersonImage = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPersonImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblLicenseID);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.pbPersonImage);
+            this.groupBox1.Controls.Add(this.lblDriver_ID);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.lblExpirationDate);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.lblDateOfBirth);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.lblIsActive);
+            this.groupBox1.Controls.Add(this.lblAppID);
+            this.groupBox1.Controls.Add(this.label18);
+            this.groupBox1.Controls.Add(this.label19);
+            this.groupBox1.Controls.Add(this.lblGender);
+            this.groupBox1.Controls.Add(this.lblPersonName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblInter_LicenseID);
+            this.groupBox1.Controls.Add(this.lblNationalNo);
+            this.groupBox1.Controls.Add(this.lblIssueDate);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(0, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(688, 212);
+            this.groupBox1.TabIndex = 106;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "International License Info ";
+            // 
+            // lblDriver_ID
+            // 
+            this.lblDriver_ID.AutoSize = true;
+            this.lblDriver_ID.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblDriver_ID.ForeColor = System.Drawing.Color.Black;
+            this.lblDriver_ID.Location = new System.Drawing.Point(414, 118);
+            this.lblDriver_ID.Name = "lblDriver_ID";
+            this.lblDriver_ID.Size = new System.Drawing.Size(23, 19);
+            this.lblDriver_ID.TabIndex = 104;
+            this.lblDriver_ID.Text = "??";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label11.Location = new System.Drawing.Point(328, 118);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(80, 19);
+            this.label11.TabIndex = 103;
+            this.label11.Text = "DriverID :";
+            // 
+            // lblExpirationDate
+            // 
+            this.lblExpirationDate.AutoSize = true;
+            this.lblExpirationDate.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExpirationDate.ForeColor = System.Drawing.Color.Black;
+            this.lblExpirationDate.Location = new System.Drawing.Point(426, 152);
+            this.lblExpirationDate.Name = "lblExpirationDate";
+            this.lblExpirationDate.Size = new System.Drawing.Size(21, 19);
+            this.lblExpirationDate.TabIndex = 102;
+            this.lblExpirationDate.Text = "??";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label13.Location = new System.Drawing.Point(284, 152);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(134, 19);
+            this.label13.TabIndex = 101;
+            this.label13.Text = "Expiration Date :";
+            // 
+            // lblDateOfBirth
+            // 
+            this.lblDateOfBirth.AutoSize = true;
+            this.lblDateOfBirth.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDateOfBirth.ForeColor = System.Drawing.Color.Black;
+            this.lblDateOfBirth.Location = new System.Drawing.Point(414, 88);
+            this.lblDateOfBirth.Name = "lblDateOfBirth";
+            this.lblDateOfBirth.Size = new System.Drawing.Size(21, 19);
+            this.lblDateOfBirth.TabIndex = 100;
+            this.lblDateOfBirth.Text = "??";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label15.Location = new System.Drawing.Point(284, 28);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(124, 19);
+            this.label15.TabIndex = 99;
+            this.label15.Text = "Application ID :";
+            // 
+            // lblIsActive
+            // 
+            this.lblIsActive.AutoSize = true;
+            this.lblIsActive.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblIsActive.ForeColor = System.Drawing.Color.Black;
+            this.lblIsActive.Location = new System.Drawing.Point(414, 58);
+            this.lblIsActive.Name = "lblIsActive";
+            this.lblIsActive.Size = new System.Drawing.Size(23, 19);
+            this.lblIsActive.TabIndex = 98;
+            this.lblIsActive.Text = "??";
+            // 
+            // lblAppID
+            // 
+            this.lblAppID.AutoSize = true;
+            this.lblAppID.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppID.ForeColor = System.Drawing.Color.Black;
+            this.lblAppID.Location = new System.Drawing.Point(414, 28);
+            this.lblAppID.Name = "lblAppID";
+            this.lblAppID.Size = new System.Drawing.Size(21, 19);
+            this.lblAppID.TabIndex = 97;
+            this.lblAppID.Text = "??";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label18.Location = new System.Drawing.Point(332, 58);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(76, 19);
+            this.label18.TabIndex = 96;
+            this.label18.Text = "IsActive :";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label19.Location = new System.Drawing.Point(293, 88);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(115, 19);
+            this.label19.TabIndex = 95;
+            this.label19.Text = "Date Of Birth :";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblGender.ForeColor = System.Drawing.Color.Black;
+            this.lblGender.Location = new System.Drawing.Point(138, 146);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(23, 19);
+            this.lblGender.TabIndex = 90;
+            this.lblGender.Text = "??";
+            // 
+            // lblPersonName
+            // 
+            this.lblPersonName.AutoSize = true;
+            this.lblPersonName.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblPersonName.ForeColor = System.Drawing.Color.Red;
+            this.lblPersonName.Location = new System.Drawing.Point(76, 28);
+            this.lblPersonName.Name = "lblPersonName";
+            this.lblPersonName.Size = new System.Drawing.Size(23, 19);
+            this.lblPersonName.TabIndex = 88;
+            this.lblPersonName.Text = "??";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(41, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 19);
+            this.label1.TabIndex = 89;
+            this.label1.Text = "Issue Date :";
+            // 
+            // lblInter_LicenseID
+            // 
+            this.lblInter_LicenseID.AutoSize = true;
+            this.lblInter_LicenseID.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInter_LicenseID.ForeColor = System.Drawing.Color.Black;
+            this.lblInter_LicenseID.Location = new System.Drawing.Point(140, 58);
+            this.lblInter_LicenseID.Name = "lblInter_LicenseID";
+            this.lblInter_LicenseID.Size = new System.Drawing.Size(21, 19);
+            this.lblInter_LicenseID.TabIndex = 87;
+            this.lblInter_LicenseID.Text = "??";
+            // 
+            // lblNationalNo
+            // 
+            this.lblNationalNo.AutoSize = true;
+            this.lblNationalNo.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblNationalNo.ForeColor = System.Drawing.Color.Black;
+            this.lblNationalNo.Location = new System.Drawing.Point(140, 118);
+            this.lblNationalNo.Name = "lblNationalNo";
+            this.lblNationalNo.Size = new System.Drawing.Size(23, 19);
+            this.lblNationalNo.TabIndex = 86;
+            this.lblNationalNo.Text = "??";
+            // 
+            // lblIssueDate
+            // 
+            this.lblIssueDate.AutoSize = true;
+            this.lblIssueDate.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIssueDate.ForeColor = System.Drawing.Color.Black;
+            this.lblIssueDate.Location = new System.Drawing.Point(138, 178);
+            this.lblIssueDate.Name = "lblIssueDate";
+            this.lblIssueDate.Size = new System.Drawing.Size(21, 19);
+            this.lblIssueDate.TabIndex = 85;
+            this.lblIssueDate.Text = "??";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(7, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 19);
+            this.label5.TabIndex = 81;
+            this.label5.Text = "Inter.LicenseID :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(31, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 19);
+            this.label7.TabIndex = 83;
+            this.label7.Text = "NationalNo :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(7, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 19);
+            this.label4.TabIndex = 80;
+            this.label4.Text = "Name :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(64, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 19);
+            this.label6.TabIndex = 82;
+            this.label6.Text = "Gender :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(47, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 19);
+            this.label3.TabIndex = 106;
+            this.label3.Text = "LicenseID :";
+            // 
+            // lblLicenseID
+            // 
+            this.lblLicenseID.AutoSize = true;
+            this.lblLicenseID.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLicenseID.ForeColor = System.Drawing.Color.Black;
+            this.lblLicenseID.Location = new System.Drawing.Point(142, 88);
+            this.lblLicenseID.Name = "lblLicenseID";
+            this.lblLicenseID.Size = new System.Drawing.Size(21, 19);
+            this.lblLicenseID.TabIndex = 107;
+            this.lblLicenseID.Text = "??";
+            // 
+            // pbPersonImage
+            // 
+            this.pbPersonImage.Location = new System.Drawing.Point(538, 28);
+            this.pbPersonImage.Name = "pbPersonImage";
+            this.pbPersonImage.Size = new System.Drawing.Size(135, 137);
+            this.pbPersonImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPersonImage.TabIndex = 105;
+            this.pbPersonImage.TabStop = false;
+            // 
+            // ctrlInternationalLicenseInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ctrlInternationalLicenseInfo";
+            this.Size = new System.Drawing.Size(690, 224);
+            this.Load += new System.EventHandler(this.ctrlInternationalLicenseInfo_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPersonImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbPersonImage;
+        private System.Windows.Forms.Label lblDriver_ID;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblExpirationDate;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblDateOfBirth;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label lblIsActive;
+        private System.Windows.Forms.Label lblAppID;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblPersonName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInter_LicenseID;
+        private System.Windows.Forms.Label lblNationalNo;
+        private System.Windows.Forms.Label lblIssueDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblLicenseID;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ DVLD/Licenses/Control/ctrlInternationalLicenseInfo.cs
@@ -0,0 +1,82 @@
+﻿using DVLD.Properties;
+using DVLD_BUSINESS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Licenses.Control
+{
+    public partial class ctrlInternationalLicenseInfo : UserControl
+    {
+        private int _InternationalLicenseID;
+        private clsInternationalLicense _InternationalLicense;
+
+        public ctrlInternationalLicenseInfo()
+        {
+            InitializeComponent();
+        }
+        public int InternationalLicenseID
+        {
+            get { return _InternationalLicenseID; }
+        }
+        private void _LoadPersonImage()
+        {
+            //initail Image
+            if (_InternationalLicense.DriverInfo.PersonInfo.Gender == 0)
+                pbPersonImage.Image = Resources.man;
+            else
+                pbPersonImage.Image = Resources.women;
+
+            string ImagePath = _InternationalLicense.DriverInfo.PersonInfo.ImagePath;
+
+            if (ImagePath != "")
+                if (File.Exists(ImagePath))
+                    pbPersonImage.Load(ImagePath);
+                else
+                    MessageBox.Show("Could not find this image: = " + ImagePath, "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+        public void LoadInfo(int International_LicenseID)
+        {
+            _InternationalLicenseID = International_LicenseID ;
+            _InternationalLicense = clsInternationalLicense.Find(_InternationalLicenseID);
+            if (_InternationalLicense == null)
+            {
+                MessageBox.Show("Could not find Internationa License ID = " + _InternationalLicenseID.ToString(),
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                _InternationalLicenseID = -1;
+                return;
+            }
+
+            lblPersonName.Text = _InternationalLicense.DriverInfo.PersonInfo.Fullname;
+            lblLicenseID.Text = _InternationalLicense.IssuedUsingLocalLicenseID.ToString();
+            lblInter_LicenseID.Text = _InternationalLicenseID.ToString();     
+            lblNationalNo.Text = _InternationalLicense.DriverInfo.PersonInfo.NationalNo;
+            lblGender.Text = _InternationalLicense.DriverInfo.PersonInfo.Gender == 0 ? "Male" : "Female";
+            lblIssueDate.Text = _InternationalLicense.IssueDate.ToString("dd MMM yyyy");
+            lblIsActive.Text = _InternationalLicense.IsActive == true ? "Yes" : "No";
+            lblAppID.Text = _InternationalLicense.ApplicationID.ToString();
+            lblDateOfBirth.Text = _InternationalLicense.DriverInfo.PersonInfo.DateOfBirth.ToString("dd MMM yyyy");
+            lblExpirationDate.Text = _InternationalLicense.ExpirationDate.ToString("dd MMM yyyy");
+
+            lblDriver_ID.Text = _InternationalLicense.DriverID.ToString();
+
+
+            _LoadPersonImage();
+
+        }
+
+        private void ctrlInternationalLicenseInfo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
