7ddae9a90740272589bddfdb06187aa938176996
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-07-13 10:43:52 +0100

    Store remembered login credentials in Windows Registry

--- DVLD/Globle Classes/GlobalClass.cs
+++ DVLD/Globle Classes/GlobalClass.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Microsoft.Win32;
 namespace DVLD.Globle_Classes
 {
     internal class GlobalClass
@@ -15,35 +15,20 @@ namespace DVLD.Globle_Classes
 
         public static bool RememberUsernameAndPassword(string Username, string Password)
         {
+            string KeyPath = @"HKEY_CURRENT_USER\Software\DVLD";
+
+            string UserNameTitel = "UserName";
+            string UserNameData = Username;
+            string PasswordTitel = "Password";
+            string PasswordData = Password;
 
             try
             {
-                //this will get the current project directory folder.
-                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+               Registry.SetValue(KeyPath,UserNameTitel,UserNameData,RegistryValueKind.String);
+               Registry.SetValue(KeyPath, PasswordTitel, PasswordData, RegistryValueKind.String);
 
+                return true;
 
-                // Define the path to the text file where you want to save the data
-                string filePath = currentDirectory + "\\data.txt";
-
-                //incase the username is empty, delete the file
-                if (Username == "" && File.Exists(filePath))
-                {
-                    File.Delete(filePath);
-                    return true;
-
-                }
-
-                // concatonate username and passwrod withe seperator.
-                string dataToSave = Username + "#//#" + Password;
-
-                // Create a StreamWriter to write to the file
-                using (StreamWriter writer = new StreamWriter(filePath))
-                {
-                    // Write the data to the file
-                    writer.WriteLine(dataToSave);
-
-                    return true;
-                }
             }
             catch (Exception ex)
             {
@@ -56,37 +41,16 @@ namespace DVLD.Globle_Classes
         public static bool GetStoredCredential(ref string Username, ref string Password)
         {
             //this will get the stored username and password and will return true if found and false if not found.
+            string keyPath = @"HKEY_CURRENT_USER\Software\DVLD";
+            string PasswordTitel = "Password";
+            string UserNameTitel = "UserName";
+
             try
             {
-                //gets the current project's directory
-                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
-
-                // Path for the file that contains the credential.
-                string filePath = currentDirectory + "\\data.txt";
-
-                // Check if the file exists before attempting to read it
-                if (File.Exists(filePath))
-                {
-                    // Create a StreamReader to read from the file
-                    using (StreamReader reader = new StreamReader(filePath))
-                    {
-                        // Read data line by line until the end of the file
-                        string line;
-                        while ((line = reader.ReadLine()) != null)
-                        {
-                            Console.WriteLine(line); // Output each line of data to the console
-                            string[] result = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
-
-                            Username = result[0];
-                            Password = result[1];
-                        }
-                        return true;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
+                 Username = Registry.GetValue(keyPath, UserNameTitel, null) as string;
+                 Password = Registry.GetValue(keyPath, PasswordTitel, null) as string;
+                return true;
+                   
             }
             catch (Exception ex)
             {
